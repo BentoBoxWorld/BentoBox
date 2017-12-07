6d7e73d8b2c54b095cb34af4a60097dab492d1d0
Junhyeong Lim <entrypoint_@naver.com>
2017-12-08 02:12:46 +0900

    Fix IOException: Stream closed

--- src/main/java/us/tastybento/bskyblock/config/YamlResourceBundle.java
+++ src/main/java/us/tastybento/bskyblock/config/YamlResourceBundle.java
@@ -3,9 +3,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *     
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -167,7 +167,7 @@ public class YamlResourceBundle extends ResourceBundle {
             String resourceName = toResourceName(bundleName, format);
             InputStream stream = loader.getResourceAsStream(resourceName);
             try {
-                return new YamlResourceBundle(stream);
+                return stream != null ? new YamlResourceBundle(stream) : null;
             } finally {
                 if (stream != null) {
                     stream.close();
