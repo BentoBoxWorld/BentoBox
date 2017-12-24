5a5aff32c0750dd567dc422da779185a1b48a987
Tastybento <tastybento@wasteofplastic.com>
2017-12-23 22:02:15 -0800

    Removed unused import.

--- src/test/java/bskyblock/TestIslandCommand.java
+++ src/test/java/bskyblock/TestIslandCommand.java
@@ -3,7 +3,6 @@ package bskyblock;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
