caa25019681608fb42c3b7192264d4b00c036d5c
Florian CUNY <poslovitch@gmail.com>
2018-06-02 22:01:05 +0200

    Removed the help item from the SettingsPanel

--- src/main/java/us/tastybento/bskyblock/panels/SettingsPanel.java
+++ src/main/java/us/tastybento/bskyblock/panels/SettingsPanel.java
@@ -20,14 +20,6 @@ public class SettingsPanel {
         PanelBuilder panelBuilder = new PanelBuilder()
                 .name(user.getTranslation("protection.panel.title"));
 
-        // Make the help item
-        PanelItem help = new PanelItemBuilder()
-                .name(user.getTranslation("protection.panel.help-item.name"))
-                .icon("MHF_Question") // Question marks
-                .build();
-        // Place it at position 8 (end of first row)
-        panelBuilder.item(8, help);
-
         // Add flags after position 8, i.e., from second row
         plugin.getFlagsManager().getFlags().forEach((f -> panelBuilder.item(f.toPanelItem(plugin, user))));
 
