66f3b5032d9d8fc182e601022bcda4240ca3c253
tastybento <tastybento@wasteofplastic.com>
2018-07-04 18:32:43 -0700

    Added protection for plates and buttons from arrows
    Added button text for flag.

--- src/main/java/us/tastybento/bskyblock/listeners/flags/PhysicalInteractionListener.java
+++ src/main/java/us/tastybento/bskyblock/listeners/flags/PhysicalInteractionListener.java
@@ -1,11 +1,15 @@
 package us.tastybento.bskyblock.listeners.flags;
 
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import us.tastybento.bskyblock.api.flags.AbstractFlagListener;
+import us.tastybento.bskyblock.api.user.User;
 import us.tastybento.bskyblock.lists.Flags;
 
 /**
@@ -20,7 +24,7 @@ public class PhysicalInteractionListener extends AbstractFlagListener {
      * @param e - event
      */
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    public void onPlayerInteract(final PlayerInteractEvent e) {
+    public void onPlayerInteract(PlayerInteractEvent e) {
         if (!e.getAction().equals(Action.PHYSICAL)) {
             return;
         }
@@ -41,4 +45,33 @@ public class PhysicalInteractionListener extends AbstractFlagListener {
 
         }
     }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onProjectileHit(EntityInteractEvent e) {
+        if (e.getEntity() == null || !(e.getEntity() instanceof Projectile)) {
+            return;
+        }
+        Projectile p = (Projectile)e.getEntity();
+        if (p.getShooter() != null && p.getShooter() instanceof Player && e.getBlock() != null) {
+            // Set the user to the shooter
+            setUser(User.getInstance((Player)p.getShooter()));
+
+            switch(e.getBlock().getType()) {
+            case WOOD_BUTTON:
+            case STONE_BUTTON:
+                checkIsland(e, e.getBlock().getLocation(), Flags.BUTTON);
+                break;
+            case WOOD_PLATE:
+            case STONE_PLATE:
+            case GOLD_PLATE:
+            case IRON_PLATE:
+                // Pressure plates
+                checkIsland(e, e.getBlock().getLocation(), Flags.PRESSURE_PLATE);
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
 }
