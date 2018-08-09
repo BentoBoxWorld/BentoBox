6d5518e97c6a4d50d939710620a02b7d8e5bec32
Florian CUNY <poslovitch@gmail.com>
2018-08-09 11:53:05 +0200

    Javadoc in PremadeEvent

--- src/main/java/world/bentobox/bentobox/api/events/PremadeEvent.java
+++ src/main/java/world/bentobox/bentobox/api/events/PremadeEvent.java
@@ -3,6 +3,9 @@ package world.bentobox.bentobox.api.events;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+/**
+ * Provides the default methods expected when extending {@link Event}.
+ */
 public abstract class PremadeEvent extends Event {
 
     private static final HandlerList handlers = new HandlerList();
