edd64d2f306fd0b2a334d28440d731d3b405cbb9
Florian CUNY <poslovitch@gmail.com>
2018-09-15 12:11:23 +0200

    Moved AddonState in Addon.State

--- src/main/java/world/bentobox/bentobox/api/addons/Addon.java
+++ src/main/java/world/bentobox/bentobox/api/addons/Addon.java
@@ -42,6 +42,38 @@ public abstract class Addon implements AddonInterface {
     }
 
     /**
+     * Represents the current run-time state of a {@link Addon}.
+     *
+     * @author Poslovitch
+     */
+    enum State {
+        /**
+         * The addon has been correctly enabled and is now fully working.
+         */
+        ENABLED,
+
+        /**
+         * The addon is fully disabled.
+         */
+        DISABLED,
+
+        /**
+         * The addon has not been loaded because it requires a different version of BentoBox or of the server software.
+         */
+        INCOMPATIBLE,
+
+        /**
+         * The addon has not been enabled because a dependency is missing.
+         */
+        MISSING_DEPENDENCY,
+
+        /**
+         * The addon loading or enabling process has been interrupted by an unhandled error.
+         */
+        ERROR
+    }
+
+    /**
      * @return the addon's default config file
      */
     public FileConfiguration getConfig() {
