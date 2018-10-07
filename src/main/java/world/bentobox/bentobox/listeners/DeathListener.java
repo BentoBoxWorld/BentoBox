a6d25d68a9a964a3de7976fb705cbb2589d85a1c
Florian CUNY <poslovitch@gmail.com>
2018-10-07 10:35:15 +0200

    Only count death if it is enabled for this world
    #164

--- src/main/java/world/bentobox/bentobox/listeners/DeathListener.java
+++ src/main/java/world/bentobox/bentobox/listeners/DeathListener.java
@@ -26,7 +26,7 @@ public class DeathListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onPlayerDeathEvent(PlayerDeathEvent e) {
-        if (plugin.getIWM().inWorld(e.getEntity().getLocation())) {
+        if (plugin.getIWM().inWorld(e.getEntity().getLocation()) && plugin.getIWM().getWorldSettings(e.getEntity().getLocation().getWorld()).isDeathsCounted()) {
             plugin.getPlayers().addDeath(e.getEntity().getWorld(), e.getEntity().getUniqueId());
         }
     }
