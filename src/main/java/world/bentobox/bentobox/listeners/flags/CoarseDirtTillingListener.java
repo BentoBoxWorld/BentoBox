23af6e7012069f0b1c5676425bfd16fd551bb61a
Florian CUNY <poslovitch@gmail.com>
2018-11-11 10:22:18 +0100

    Few fixes to COARSE_DIRT_TILLING flag
    #337

--- src/main/java/world/bentobox/bentobox/listeners/flags/CoarseDirtTillingListener.java
+++ src/main/java/world/bentobox/bentobox/listeners/flags/CoarseDirtTillingListener.java
@@ -6,13 +6,15 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import world.bentobox.bentobox.api.flags.FlagListener;
+import world.bentobox.bentobox.api.localization.TextVariables;
+import world.bentobox.bentobox.api.user.User;
 import world.bentobox.bentobox.lists.Flags;
 
 public class CoarseDirtTillingListener extends FlagListener {
 
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onTillingCoarseDirt(PlayerInteractEvent e) {
-        if (!e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+        if (!e.getAction().equals(Action.RIGHT_CLICK_BLOCK) || e.getItem() == null) {
             return;
         }
 
@@ -26,7 +28,9 @@ public class CoarseDirtTillingListener extends FlagListener {
                     if (e.getClickedBlock().getType().equals(Material.COARSE_DIRT)
                             && getIWM().inWorld(e.getClickedBlock().getWorld())
                             && !Flags.COARSE_DIRT_TILLING.isSetForWorld(e.getClickedBlock().getWorld())) {
-                        noGo(e, Flags.COARSE_DIRT_TILLING);
+                        e.setCancelled(true);
+                        User user = User.getInstance(e.getPlayer());
+                        user.notify("protection.protected", TextVariables.DESCRIPTION, user.getTranslation(Flags.COARSE_DIRT_TILLING.getHintReference()));
                     }
                     break;
                 default:
