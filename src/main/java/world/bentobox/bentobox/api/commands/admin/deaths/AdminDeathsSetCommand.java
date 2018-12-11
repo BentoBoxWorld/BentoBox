486de0dc1904c67c286f3f94747e4af304a43c8f
Florian CUNY <poslovitch@gmail.com>
2018-12-11 14:51:05 +0100

    Added admin command to reset/set deaths
    #76
    * Added AdminDeathsCommand
    * Added AdminDeathsResetCommand
    * Added AdminDeathsSetCommand
    * Updated en-US locale accordingly
    * Added "general.errors.must-be-positive-number" to the en-US locale
    * Fixed some formatting in the en-US locale

---
+++ src/main/java/world/bentobox/bentobox/api/commands/admin/deaths/AdminDeathsSetCommand.java
@@ -0,0 +1,46 @@
+package world.bentobox.bentobox.api.commands.admin.deaths;
+
+import org.apache.commons.lang.math.NumberUtils;
+import world.bentobox.bentobox.api.commands.CompositeCommand;
+import world.bentobox.bentobox.api.localization.TextVariables;
+import world.bentobox.bentobox.api.user.User;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author Poslovitch
+ */
+public class AdminDeathsSetCommand extends CompositeCommand {
+
+    public AdminDeathsSetCommand(AdminDeathsCommand parent) {
+        super(parent, "set");
+    }
+
+    @Override
+    public void setup() {
+        setDescription("commands.admin.deaths.set.description");
+        setParametersHelp("commands.admin.deaths.set.parameters");
+    }
+
+    @Override
+    public boolean execute(User user, String label, List<String> args) {
+        if (args.isEmpty() || args.size() != 2) {
+            showHelp(this, user);
+            return false;
+        }
+
+        UUID target = getPlayers().getUUID(args.get(0));
+        if (target == null) {
+            user.sendMessage("general.errors.unknown-player", TextVariables.NAME, args.get(0));
+        } else if (!NumberUtils.isNumber(args.get(1)) || Integer.valueOf(args.get(1)) < 0) {
+            user.sendMessage("general.errors.must-be-positive-number", TextVariables.NUMBER, args.get(1));
+        } else {
+            getPlayers().setDeaths(getWorld(), target, Integer.valueOf(args.get(1)));
+            user.sendMessage("general.success");
+            return true;
+        }
+
+        return false;
+    }
+}
