96bf67eb1961c9deb7f62d5e33d326b6ddf124b1
tastybento <tastybento@wasteofplastic.com>
2019-01-02 15:40:49 -0800

    Added generic reload command for addons to use
    This command just runs the onReload method so that individual addons can
    reload their stuff.

---
+++ src/main/java/world/bentobox/bentobox/api/commands/admin/AdminReloadCommand.java
@@ -0,0 +1,33 @@
+package world.bentobox.bentobox.api.commands.admin;
+
+import java.util.List;
+
+import world.bentobox.bentobox.api.commands.CompositeCommand;
+import world.bentobox.bentobox.api.user.User;
+
+/**
+ * Generic reload command for addons to use that will just call the onReload() method
+ * after checking permissions
+ * @author tastybento
+ *
+ */
+public class AdminReloadCommand extends CompositeCommand {
+
+    public AdminReloadCommand(CompositeCommand adminCommand) {
+        super(adminCommand, "reload");
+    }
+
+    @Override
+    public void setup() {
+        // Permission
+        setPermission("admin.reload");
+        setDescription("commands.admin.reload.description");
+    }
+
+    @Override
+    public boolean execute(User user, String label, List<String> args) {
+        getAddon().onReload();
+        return true;
+    }
+
+}
