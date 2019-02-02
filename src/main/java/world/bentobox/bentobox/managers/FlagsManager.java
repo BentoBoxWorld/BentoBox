fd3815d13212dd92df790f6bb6d7b848d077caa7
Florian CUNY <poslovitch@gmail.com>
2019-02-02 11:29:53 +0100

    Removed unused import in FlagsManager

--- src/main/java/world/bentobox/bentobox/managers/FlagsManager.java
+++ src/main/java/world/bentobox/bentobox/managers/FlagsManager.java
@@ -10,7 +10,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 
 import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
 import world.bentobox.bentobox.BentoBox;
 import world.bentobox.bentobox.api.flags.Flag;
 import world.bentobox.bentobox.lists.Flags;
