8786c3a8791f332ad468b3eff9bf7718e7defcd6
Florian CUNY <poslovitch@gmail.com>
2019-02-06 15:28:09 +0100

    Fixed /bbox v using [name] textvariable to display BentoBox's version instead of [version]

--- src/main/java/world/bentobox/bentobox/commands/BentoBoxVersionCommand.java
+++ src/main/java/world/bentobox/bentobox/commands/BentoBoxVersionCommand.java
@@ -28,12 +28,11 @@ public class BentoBoxVersionCommand extends CompositeCommand {
 
     @Override
     public boolean execute(User user, String label, List<String> args) {
-        user.sendMessage("commands.bentobox.version.plugin-version", TextVariables.NAME, getPlugin().getDescription().getVersion());
+        user.sendMessage("commands.bentobox.version.plugin-version", TextVariables.VERSION, getPlugin().getDescription().getVersion());
         user.sendMessage("commands.bentobox.version.loaded-game-worlds");
         getIWM().getOverWorldNames().forEach((k,v) -> user.sendMessage("commands.bentobox.version.game-worlds", TextVariables.NAME, k, "[addon]", v));
         user.sendMessage("commands.bentobox.version.loaded-addons");
-        getPlugin().getAddonsManager()
-        .getAddons()
+        getPlugin().getAddonsManager().getAddons()
         .forEach(a -> user.sendMessage("commands.bentobox.version.addon-syntax", TextVariables.NAME, a.getDescription().getName(),
                 TextVariables.VERSION, a.getDescription().getVersion()));
 
