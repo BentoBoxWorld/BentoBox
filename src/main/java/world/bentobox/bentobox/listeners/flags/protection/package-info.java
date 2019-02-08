1e20ccb6040bdd88397c7844ff772e2505cf27c6
Florian CUNY <poslovitch@gmail.com>
2019-02-08 20:56:53 +0100

    Added package-info.java to flag listeners packages

---
+++ src/main/java/world/bentobox/bentobox/listeners/flags/protection/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains listeners that handle {@link world.bentobox.bentobox.api.flags.Flag.Type#PROTECTION PROTECTION} flags.
+ * @since 1.3.0
+ */
+package world.bentobox.bentobox.listeners.flags.protection;
