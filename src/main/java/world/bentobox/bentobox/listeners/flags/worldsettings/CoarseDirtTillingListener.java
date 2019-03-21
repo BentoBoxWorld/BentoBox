99e9ad6ac8dca9cc9eea6a35ac80401fa76b1821
tastybento <tastybento@wasteofplastic.com>
2019-03-20 23:38:02 -0700

    Makes podzol yield podzol when broken if coarse dirt tilling is denied
    https://github.com/BentoBoxWorld/BentoBox/issues/613

--- src/main/java/world/bentobox/bentobox/listeners/flags/worldsettings/CoarseDirtTillingListener.java
+++ src/main/java/world/bentobox/bentobox/listeners/flags/worldsettings/CoarseDirtTillingListener.java
@@ -1,10 +1,13 @@
 package world.bentobox.bentobox.listeners.flags.worldsettings;
 
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 import world.bentobox.bentobox.api.flags.FlagListener;
 import world.bentobox.bentobox.api.localization.TextVariables;
@@ -21,22 +24,39 @@ public class CoarseDirtTillingListener extends FlagListener {
 
         if (e.getClickedBlock().getType().equals(Material.COARSE_DIRT)) {
             switch (e.getItem().getType()) {
-                case WOODEN_HOE:
-                case STONE_HOE:
-                case IRON_HOE:
-                case GOLDEN_HOE:
-                case DIAMOND_HOE:
-                    if (e.getClickedBlock().getType().equals(Material.COARSE_DIRT)
-                            && getIWM().inWorld(e.getClickedBlock().getWorld())
-                            && !Flags.COARSE_DIRT_TILLING.isSetForWorld(e.getClickedBlock().getWorld())) {
-                        e.setCancelled(true);
-                        User user = User.getInstance(e.getPlayer());
-                        user.notify("protection.protected", TextVariables.DESCRIPTION, user.getTranslation(Flags.COARSE_DIRT_TILLING.getHintReference()));
-                    }
-                    break;
-                default:
-                    break;
+            case WOODEN_HOE:
+            case STONE_HOE:
+            case IRON_HOE:
+            case GOLDEN_HOE:
+            case DIAMOND_HOE:
+                if (e.getClickedBlock().getType().equals(Material.COARSE_DIRT)
+                        && getIWM().inWorld(e.getClickedBlock().getWorld())
+                        && !Flags.COARSE_DIRT_TILLING.isSetForWorld(e.getClickedBlock().getWorld())) {
+                    e.setCancelled(true);
+                    User user = User.getInstance(e.getPlayer());
+                    user.notify("protection.protected", TextVariables.DESCRIPTION, user.getTranslation(Flags.COARSE_DIRT_TILLING.getHintReference()));
+                }
+                break;
+            default:
+                break;
             }
+        }
+    }
+
+    /**
+     * If podzol is mined when coarse dirt tilling is not allowed, then it'll just drop podzol and not dirt
+     * This prevents an exploit where growing big spruce trees can turn gravel into podzol.
+     * https://github.com/BentoBoxWorld/BentoBox/issues/613
+     * @param e - event
+     */
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onBreakingPodzol(BlockBreakEvent e) {
+        if (!e.getPlayer().getGameMode().equals(GameMode.CREATIVE)
+                && e.getBlock().getType().equals(Material.PODZOL)
+                && getIWM().inWorld(e.getBlock().getWorld())
+                && !Flags.COARSE_DIRT_TILLING.isSetForWorld(e.getBlock().getWorld())) {
+            e.getBlock().setType(Material.AIR);
+            e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.PODZOL));
         }
     }
 }
