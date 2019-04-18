c47a41f3fc3376295b130f8e9a5593e5c017fb4e
Florian CUNY <poslovitch@bentobox.world>
2019-04-18 14:41:29 +0200

    Removed the drafty Github api - we now use the GitHubAPI4Java :)

--- src/main/java/world/bentobox/bentobox/api/github/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Contains objects and classes to get, retrieve and store data from GitHub.
- * @since 1.3.0
- */
-package world.bentobox.bentobox.api.github;
--- src/main/java/world/bentobox/bentobox/managers/WebManager.java
+++ src/main/java/world/bentobox/bentobox/managers/WebManager.java
@@ -1,15 +1,7 @@
 package world.bentobox.bentobox.managers;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import org.bukkit.Bukkit;
 import org.eclipse.jdt.annotation.NonNull;
-
-import org.eclipse.jdt.annotation.Nullable;
 import world.bentobox.bentobox.BentoBox;
-import world.bentobox.bentobox.api.addons.Addon;
-import world.bentobox.bentobox.api.github.GitHubConnector;
 
 /**
  * Handles web-related stuff.
@@ -20,13 +12,10 @@ import world.bentobox.bentobox.api.github.GitHubConnector;
 public class WebManager {
 
     private @NonNull BentoBox plugin;
-    private @Nullable GitHubConnector bentoBoxGitHubConnector;
-    private @NonNull Map<@NonNull Addon, @NonNull GitHubConnector> gitHubConnectors = new LinkedHashMap<>();
 
     public WebManager(@NonNull BentoBox plugin) {
         this.plugin = plugin;
         if (plugin.getSettings().isGithubDownloadData()) {
-            this.bentoBoxGitHubConnector = new GitHubConnector("BentoBoxWorld/BentoBox");
             setupAddonsGitHubConnectors();
         }
     }
@@ -36,17 +25,13 @@ public class WebManager {
      */
     private void setupAddonsGitHubConnectors() {
         plugin.getAddonsManager().getEnabledAddons().stream()
-                .filter(addon -> !addon.getDescription().getRepository().isEmpty())
-                .forEach(addon -> gitHubConnectors.put(addon, new GitHubConnector(addon.getDescription().getRepository())));
+                .filter(addon -> !addon.getDescription().getRepository().isEmpty());
     }
 
     /**
-     * Connects all the {@link GitHubConnector} to GitHub to retrieve data.
      */
     public void requestGitHubData() {
         if (plugin.getSettings().isGithubDownloadData()) {
-            Bukkit.getScheduler().runTask(plugin, () -> bentoBoxGitHubConnector.connect());
-            gitHubConnectors.values().forEach(gitHubConnector -> Bukkit.getScheduler().runTask(plugin, gitHubConnector::connect));
         }
     }
 }
