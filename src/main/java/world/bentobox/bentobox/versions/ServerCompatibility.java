a268bf7b5290464e79e47ead5998e18988e24a81
Florian CUNY <poslovitch@bentobox.world>
2019-04-23 06:42:30 +0200

    Added 1.14 as not supported

--- src/main/java/world/bentobox/bentobox/versions/ServerCompatibility.java
+++ src/main/java/world/bentobox/bentobox/versions/ServerCompatibility.java
@@ -94,7 +94,11 @@ public class ServerCompatibility {
     public enum ServerVersion {
         V1_13(Compatibility.NOT_SUPPORTED),
         V1_13_1(Compatibility.NOT_SUPPORTED),
-        V1_13_2(Compatibility.COMPATIBLE);
+        V1_13_2(Compatibility.COMPATIBLE),
+        /*
+         * @since 1.5.0
+         */
+        V1_14(Compatibility.NOT_SUPPORTED);
 
         private Compatibility compatibility;
 
