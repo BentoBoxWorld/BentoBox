d1e91889597add1cb209cc14742043b0de56cb14
BONNe <bonne@bonne.id.lv>
2019-06-14 14:59:47 +0300

    Added ability to explicitly define asynchronous PremadeEvent (#763)
    * Add ability to define asynchronous event
    
    * minor formatting
    
    * Removed another line jump

--- src/main/java/world/bentobox/bentobox/api/events/PremadeEvent.java
+++ src/main/java/world/bentobox/bentobox/api/events/PremadeEvent.java
@@ -8,6 +8,24 @@ import org.bukkit.event.HandlerList;
  */
 public abstract class PremadeEvent extends Event {
 
+    /**
+     * The default constructor is defined for cleaner code.
+     * This constructor assumes the PremadeEvent is synchronous.
+     */
+    public PremadeEvent() {
+        this(false);
+    }
+
+    /**
+     * This constructor is used to explicitly declare an PremadeEvent as synchronous or asynchronous.
+     * @param async - true indicates the event will fire asynchronously, false
+     *    by default from default constructor
+     * @since 1.5.2
+     */
+    public PremadeEvent(boolean async) {
+        super(async);
+    }
+
     private static final HandlerList handlers = new HandlerList();
 
     @Override
