2484421d9227f4d4bfa1157777ce2da3f27f122a
Leonardo Chaia <leonardochaia@users.noreply.github.com>
2019-10-13 11:11:59 -0300

    Allows customizing where islands are created (#984)
    Implements https://github.com/BentoBoxWorld/BentoBox/issues/983.
    
    * Allows customizing where islands are created
    
    NewIsland receives a new interface which determines the location
    where the next island will be created
    
    A default implementation is provided which mirrors the previous behavior
    
    * Adds autors to DefaultNewIslandLocationStrategy
    
    Co-Authored-By: Florian CUNY <poslovitch@bentobox.world>
    
    * Improves Javadocs
    
    Co-Authored-By: Florian CUNY <poslovitch@bentobox.world>

---
+++ src/main/java/world/bentobox/bentobox/managers/island/NewIslandLocationStrategy.java
@@ -0,0 +1,14 @@
+package world.bentobox.bentobox.managers.island;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+
+/**
+ * Determines the locations for new islands
+ * @author tastybento, leonardochaia
+ * @since 1.8.0
+ *
+ */
+public interface NewIslandLocationStrategy {
+    public Location getNextLocation(World world);
+}
