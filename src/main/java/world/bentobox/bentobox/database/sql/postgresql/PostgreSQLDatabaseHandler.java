76df0a621abf68fcbabb84a2853bf19a75a8af30
tastybento <tastybento@wasteofplastic.com>
2020-01-22 16:35:56 -0800

    JSON based PostGresql

--- src/main/java/world/bentobox/bentobox/database/sql/postgresql/PostgreSQLDatabaseHandler.java
+++ src/main/java/world/bentobox/bentobox/database/sql/postgresql/PostgreSQLDatabaseHandler.java
@@ -1,7 +1,13 @@
 package world.bentobox.bentobox.database.sql.postgresql;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.google.gson.Gson;
+
 import world.bentobox.bentobox.BentoBox;
 import world.bentobox.bentobox.database.DatabaseConnector;
+import world.bentobox.bentobox.database.objects.DataObject;
 import world.bentobox.bentobox.database.sql.SQLConfiguration;
 import world.bentobox.bentobox.database.sql.SQLDatabaseHandler;
 
@@ -24,17 +30,46 @@ public class PostgreSQLDatabaseHandler<T> extends SQLDatabaseHandler<T> {
      */
     PostgreSQLDatabaseHandler(BentoBox plugin, Class<T> type, DatabaseConnector databaseConnector) {
         super(plugin, type, databaseConnector, new SQLConfiguration(type.getCanonicalName())
-                .schema("CREATE TABLE IF NOT EXISTS `" + type.getCanonicalName() 
-                        + "` (uniqueId VARCHAR(255) NOT NULL PRIMARY KEY, json json NOT NULL)")
-                .saveObject("INSERT INTO `" + type.getCanonicalName() + "` (json) VALUES (?) ON CONFLICT (uniqueId) DO UPDATE " +
-                        "SET json = ?")
+                .schema("CREATE TABLE IF NOT EXISTS \"" + type.getCanonicalName() + "\" (json jsonb NOT NULL)")
+                .loadObject("SELECT * FROM \"" + type.getCanonicalName() + "\" WHERE json->>'uniqueId' = ?")
+                .deleteObject("DELETE FROM \"" + type.getCanonicalName() + "\" WHERE json->>'uniqueId' = ?")
+                .saveObject("INSERT INTO \"" + type.getCanonicalName() + "\" (json) VALUES (cast(? as json))")
+                .loadObjects("SELECT json FROM \"" + type.getCanonicalName() + "\"")
+                .objectExists("SELECT EXISTS(SELECT * FROM \"" + type.getCanonicalName() + "\" WHERE json->>'uniqueId' = ?)")
                 );
+        // Create index
         /*
-         * TODO complete
-        saveObjectSQL = "INSERT INTO `" + canonicalName + "` (json) VALUES (?) ON DUPLICATE KEY UPDATE json = ?";
-        deleteObjectSQL = "DELETE FROM `" + canonicalName + "` WHERE uniqueId = ?";
-        objectExistsSQL = "SELECT IF ( EXISTS( SELECT * FROM `" + canonicalName + "` WHERE `uniqueId` = ?), 1, 0)";
-    */
-        }
+        try (Statement s = this.getConnection().createStatement()) {
+            s.executeQuery("CREATE INDEX idx_json ON \"" + type.getCanonicalName() + "\" USING GIN ((json->‘uniqueId’))");
+        } catch (SQLException e) {
+            plugin.logError("Could not make index in Postgresql database table for " + type.getCanonicalName());
+            e.printStackTrace();
+        }*/
+    }
 
+    @Override
+    public void saveObject(T instance) {
+        // Null check
+        if (instance == null) {
+            plugin.logError("MySQL database request to store a null. ");
+            return;
+        }
+        if (!(instance instanceof DataObject)) {
+            plugin.logError("This class is not a DataObject: " + instance.getClass().getName());
+            return;
+        }
+        Gson gson = getGson();
+        String toStore = gson.toJson(instance);
+        String uniqueId = ((DataObject)instance).getUniqueId();
+        this.deleteID(uniqueId);
+        processQueue.add(() -> {
+            try (PreparedStatement preparedStatement = getConnection().prepareStatement(getSqlConfig().getSaveObjectSQL())) {
+                preparedStatement.setString(1, toStore);
+                plugin.log(preparedStatement.toString());
+                preparedStatement.execute();
+            } catch (SQLException e) {
+                plugin.logError("Could not save object " + instance.getClass().getName() + " " + e.getMessage());
+            }
+        });
+    }
 }
