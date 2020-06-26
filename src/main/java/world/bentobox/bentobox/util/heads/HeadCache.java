51c69f34af6ca4f90013734e120fc4b42db5842e
Florian CUNY <poslovitch@bentobox.world>
2020-06-26 17:15:15 +0200

    Added some basic javadoc header to HeadCache

--- src/main/java/world/bentobox/bentobox/util/heads/HeadCache.java
+++ src/main/java/world/bentobox/bentobox/util/heads/HeadCache.java
@@ -2,6 +2,10 @@ package world.bentobox.bentobox.util.heads;
 
 import org.bukkit.inventory.ItemStack;
 
+/**
+ * @since 1.14.0
+ * @author tastybento
+ */
 public class HeadCache {
     private final ItemStack head;
     private final long timestamp;
