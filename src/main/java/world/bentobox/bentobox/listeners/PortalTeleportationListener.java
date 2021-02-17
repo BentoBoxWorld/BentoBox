819defb5f57149a6bf031c1a01be915f5d21e0ae
Huynh Tien <huynhqtienvtag@gmail.com>
2021-02-17 19:25:43 +0700

    Forgot to import

--- src/main/java/world/bentobox/bentobox/listeners/PortalTeleportationListener.java
+++ src/main/java/world/bentobox/bentobox/listeners/PortalTeleportationListener.java
@@ -12,6 +12,7 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.World.Environment;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
