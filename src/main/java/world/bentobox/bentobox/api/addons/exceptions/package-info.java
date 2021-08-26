518996b0901317275c427f09ba967249da906894
tastybento <tastybento@wasteofplastic.com>
2021-08-25 18:51:38 -0700

    WIP adding package info files for better JavaDocs

---
+++ src/main/java/world/bentobox/bentobox/api/addons/exceptions/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package covers Addon exceptions
+ * <p>
+ * These exceptions can be thrown when the addon is loaded.
+ * </p>
+ *
+ * @since 1.0
+ * @author tastybento
+ */
+package world.bentobox.bentobox.api.addons.exceptions;
