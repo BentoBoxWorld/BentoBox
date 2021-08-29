a3966e1fe2627b91d57e0f726590d8befb186dd6
tastybento <tastybento@wasteofplastic.com>
2021-08-29 15:32:01 -0700

    Fix JavaDoc issues

--- src/main/java/world/bentobox/bentobox/api/commands/admin/DefaultAdminCommand.java
+++ src/main/java/world/bentobox/bentobox/api/commands/admin/DefaultAdminCommand.java
@@ -96,7 +96,7 @@ public abstract class DefaultAdminCommand extends CompositeCommand {
 
     /**
      * Defines what will be executed when this command is run.
-     * @see world.bentobox.bentobox.api.commands.BentoBoxCommand#execute(User, String, List&lt;String&gt;)
+     * @see world.bentobox.bentobox.api.commands.BentoBoxCommand#execute(User, String, List)
      */
     @Override
     public boolean execute(User user, String label, List<String> args) {
