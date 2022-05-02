6fba4bfbea983c47125eeb6109433d95a1fa0303
Huynh Tien <huynhqtienvtag@gmail.com>
2022-05-03 05:23:31 +0700

    More abstract on World Regenerator (#1969)
    * more abstract on delete chunks
    
    * update NMS to 1.18.2
    
    * at most abstract, requires only the island and the world
    
    * it's weird that we can't use whenComplete
    
    * rename to WorldRegenerator

--- src/main/java/world/bentobox/bentobox/util/Util.java
+++ src/main/java/world/bentobox/bentobox/util/Util.java
@@ -47,7 +47,7 @@ import io.papermc.lib.PaperLib;
 import io.papermc.lib.features.blockstatesnapshot.BlockStateSnapshotResult;
 import world.bentobox.bentobox.BentoBox;
 import world.bentobox.bentobox.api.user.User;
-import world.bentobox.bentobox.nms.NMSAbstraction;
+import world.bentobox.bentobox.nms.WorldRegenerator;
 
 /**
  * A set of utility methods
@@ -64,7 +64,7 @@ public class Util {
     private static final String THE_END = "_the_end";
     private static String serverVersion = null;
     private static BentoBox plugin = BentoBox.getInstance();
-    private static NMSAbstraction nms = null;
+    private static WorldRegenerator regenerator = null;
 
     private Util() {}
 
@@ -689,23 +689,37 @@ public class Util {
     }
 
     /**
-     * Set the NMS handler the plugin will use
-     * @param nms the NMS handler
+     * Set the regenerator the plugin will use
+     * @param regenerator the regenerator
      */
-    public static void setNms(NMSAbstraction nms) {
-        Util.nms = nms;
+    public static void setRegenerator(WorldRegenerator regenerator) {
+        Util.regenerator = regenerator;
     }
 
     /**
-     * Get the NMS handler the plugin will use
-     * @return an NMS accelerated class for this server
+     * Get the regenerator the plugin will use
+     * @return an accelerated regenerator class for this server
      */
-    public static NMSAbstraction getNMS() {
-        if (nms == null) {
-            plugin.log("No NMS Handler was set, falling back to Bukkit API.");
-            setNms(new world.bentobox.bentobox.nms.fallback.NMSHandler());
+    public static WorldRegenerator getRegenerator() {
+        if (regenerator == null) {
+            String serverPackageName = Bukkit.getServer().getClass().getPackage().getName();
+            String pluginPackageName = plugin.getClass().getPackage().getName();
+            String version = serverPackageName.substring(serverPackageName.lastIndexOf('.') + 1);
+            WorldRegenerator handler;
+            try {
+                Class<?> clazz = Class.forName(pluginPackageName + ".nms." + version + ".WorldRegeneratorImpl");
+                if (WorldRegenerator.class.isAssignableFrom(clazz)) {
+                    handler = (WorldRegenerator) clazz.getConstructor().newInstance();
+                } else {
+                    throw new IllegalStateException("Class " + clazz.getName() + " does not implement WorldRegenerator");
+                }
+            } catch (Exception e) {
+                plugin.logWarning("No Regenerator found for " + version + ", falling back to Bukkit API.");
+                handler = new world.bentobox.bentobox.nms.fallback.WorldRegeneratorImpl();
+            }
+            setRegenerator(handler);
         }
-        return nms;
+        return regenerator;
     }
 
     /**
