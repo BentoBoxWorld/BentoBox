BentoBoxWorld/BentoBox

commit 85b52f4bfb46e220618fddb52b09aa6558fe4d28

Implement new 1.19 materials and entities (#1990)
* Update to Spigot and Paper API 1.19 and Java 17

* Set minimal version to 1.18.

* Add breeding protection for Axolotl and Goats (1.18)
Add breeding protection for Frog and Allay (1.19)

* Clean up switch statement in BlockInteractionListener

* Add chest boat support to BreakBlocksListener.

Part of #1987

* Add powdered snow pickup with bucket protection.

* Add glow ink sac protection on signs.
Switch to SheepDyeWoolEvent as it now contains player variable.

* Clean up Hurting Listener

* Mark 1.16.5, 1.17 and 1.17.1 as incompatible.
Mark 1.18, 1.18.1 and 1.18.2 as supported.

Add 1.19 as compatible.
Part of 1987

* Add ChestBoat inventory protection.
Part of #1987

* Clean up LockAndBanListener

* Add MANGROVE_PRESSURE_PLATE protection.
Part of #1987

* Add Glow Item Frame protection to player interact event.

* Clean up MobSpawnListener

* Clean up ChestDamageListener

* Change from custom method to Tag.PRESSURE_PLATES to detect pressure plates.
Part of #1987

* Implement proper chest boat protection.
Part of #1987

* Move to 1.19 R1 world regenerator.
Part of #1987

* Add allay to the animal entity list.
Part of #1987

* Add axolotl and other fish scooping protection.
Part of #1987

* Fixes Bucket and Glass Bottle filling.

Buckets and bottles were not working since cauldron splitting by type. This change fixes that.

It also protects from filling bottles with water from water sources or waterlogged blocks.

* Remove 1.17.1 compatibility check for biome adapter.
Add cheezee 1.19.1 compatibility version.

* Fixes chest boat interactions.

Part of #1987

* Implement Allay protection.
New flag ALLAY is required to interact with allays.

Part of #1987

* Prevent visitors for being targeted by entities if ENTITY_ATTACK is enabled.

* Implement Sculk Sensor and Shrieker activation protection setting.

Part of #1987

src/main/java/world/bentobox/bentobox/listeners/flags/worldsettings/InvincibleVisitorsListener.java
@@ -12,6 +12,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.event.inventory.ClickType;
 
 import world.bentobox.bentobox.BentoBox;
@@ -157,5 +159,28 @@ public class InvincibleVisitorsListener extends FlagListener implements ClickHan
     }
 
 
+    /**
+     * This listener cancels entity targeting a player if he is a visitor, and visitors are immune to entity damage.
+     * @param e EntityTargetLivingEntityEvent
+     */
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onVisitorTargeting(EntityTargetLivingEntityEvent e)
+    {
+        World world = e.getEntity().getWorld();
+
+        if (!(e.getTarget() instanceof Player p) ||
+            !this.getIWM().inWorld(world) ||
+            e.getTarget().hasMetadata("NPC") ||
+            this.getIslands().userIsOnIsland(world, User.getInstance(e.getEntity())) ||
+            this.PVPAllowed(p.getLocation()) ||
+            e.getReason() == EntityTargetEvent.TargetReason.TARGET_DIED ||
+            !this.getIWM().getIvSettings(world).contains(DamageCause.ENTITY_ATTACK.name()))
+        {
+            return;
+        }
+
+        // Cancel targeting event.
+        e.setCancelled(true);
+    }
 }
 

pom.xml
@@ -63,15 +63,15 @@
     <properties>
         <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
         <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
-        <java.version>16</java.version>
+        <java.version>17</java.version>
         <!-- Non-minecraft related dependencies -->
         <powermock.version>2.0.9</powermock.version>
         <mongodb.version>3.12.8</mongodb.version>
         <!-- More visible way to change dependency versions -->
-        <spigot.version>1.18.2-R0.1-SNAPSHOT</spigot.version>
+        <spigot.version>1.19-R0.1-SNAPSHOT</spigot.version>
         <!-- Might differ from the last Spigot release for short periods 
             of time -->
-        <paper.version>1.16.5-R0.1-SNAPSHOT</paper.version>
+        <paper.version>1.19-R0.1-SNAPSHOT</paper.version>
         <bstats.version>2.2.1</bstats.version>
         <vault.version>1.7</vault.version>
         <placeholderapi.version>2.10.9</placeholderapi.version>
@@ -164,7 +164,7 @@
         </repository>
         <repository>
             <id>papermc</id>
-            <url>https://papermc.io/repo/repository/maven-public/</url>
+            <url>https://repo.papermc.io/repository/maven-public/</url>
         </repository>
         <repository>
             <!-- This is a temporary reference as the Maven Shade plugin 
@@ -188,7 +188,7 @@
         </dependency>
         <!-- Paper API -->
         <dependency>
-            <groupId>com.destroystokyo.paper</groupId>
+            <groupId>io.papermc.paper</groupId>
             <artifactId>paper-api</artifactId>
             <version>${paper.version}</version>
             <scope>provided</scope>

src/main/java/world/bentobox/bentobox/database/json/BentoboxTypeAdapterFactory.java
@@ -56,7 +56,7 @@ public class BentoboxTypeAdapterFactory implements TypeAdapterFactory {
         if (Location.class.isAssignableFrom(rawType)) {
             // Use our current location adapter for backward compatibility
             return (TypeAdapter<T>) new LocationTypeAdapter();
-        } else if (Biome.class.isAssignableFrom(rawType) && !ServerCompatibility.getInstance().isVersion(ServerCompatibility.ServerVersion.V1_17_1)) { // TODO: Any better way ?
+        } else if (Biome.class.isAssignableFrom(rawType)) {
             return (TypeAdapter<T>) new BiomeTypeAdapter();
         } else if (Enum.class.isAssignableFrom(rawType)) {
             return new EnumTypeAdapter(rawType);

src/main/java/world/bentobox/bentobox/hooks/LangUtilsHook.java
@@ -626,6 +626,8 @@ public class LangUtilsHook extends Hook {
             case MUSIC_DISC_11 -> "C418 - 11";
             case MUSIC_DISC_WAIT -> "C418 - wait";
             case MUSIC_DISC_PIGSTEP -> "Lena Raine - Pigstep";
+            case MUSIC_DISC_5 -> "Samuel Åberg - 5";
+            case MUSIC_DISC_OTHERSIDE -> "Lena Raine - otherside";
             default -> null;
         };
     }

src/main/java/world/bentobox/bentobox/listeners/flags/protection/BlockInteractionListener.java
@@ -1,12 +1,15 @@
 package world.bentobox.bentobox.listeners.flags.protection;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 
+import org.bukkit.FluidCollisionMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.Waterlogged;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
@@ -26,236 +29,269 @@ import world.bentobox.bentobox.lists.Flags;
  * Handle interaction with blocks
  * @author tastybento
  */
-public class BlockInteractionListener extends FlagListener {
+public class BlockInteractionListener extends FlagListener
+{
 
     /**
-     * These cover materials in another server version.
-     * This avoids run time errors due to unknown enum values, at the expense of a string comparison
+     * These cover materials in another server version. This avoids run time errors due to unknown enum values, at the
+     * expense of a string comparison
      */
     private final static Map<String, String> stringFlags;
-    static {
-        stringFlags = Map.of("RESPAWN_ANCHOR", "PLACE_BLOCKS");
+
+    static
+    {
+        stringFlags = Map.of(
+            "ACACIA_CHEST_BOAT", "CHEST",
+            "BIRCH_CHEST_BOAT", "CHEST",
+            "JUNGLE_CHEST_BOAT", "CHEST",
+            "DARK_OAK_CHEST_BOAT", "CHEST",
+            "MANGROVE_CHEST_BOAT", "CHEST",
+            "OAK_CHEST_BOAT", "CHEST",
+            "SPRUCE_CHEST_BOAT", "CHEST");
     }
 
     /**
      * Handle interaction with blocks
+     *
      * @param e - event
      */
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    public void onPlayerInteract(final PlayerInteractEvent e) {
+    public void onPlayerInteract(final PlayerInteractEvent e)
+    {
         // We only care about the RIGHT_CLICK_BLOCK action.
-        if (!e.getAction().equals(Action.RIGHT_CLICK_BLOCK) || e.getClickedBlock() == null) {
+        if (!e.getAction().equals(Action.RIGHT_CLICK_BLOCK) || e.getClickedBlock() == null)
+        {
             return;
         }
 
         // Check clicked block
-        checkClickedBlock(e, e.getPlayer(), e.getClickedBlock().getLocation(), e.getClickedBlock().getType());
+        this.checkClickedBlock(e, e.getPlayer(), e.getClickedBlock().getLocation(), e.getClickedBlock().getType());
 
         // Now check for in-hand items
-        if (e.getItem() != null && !e.getItem().getType().equals(Material.AIR)) {
+        if (e.getItem() != null && !e.getItem().getType().equals(Material.AIR))
+        {
             // Boats
-            if (e.getItem().getType().name().endsWith("_BOAT")) {
-                checkIsland(e, e.getPlayer(), e.getClickedBlock().getLocation(), Flags.BOAT);
+            if (e.getItem().getType().name().endsWith("BOAT"))
+            {
+                this.checkIsland(e, e.getPlayer(), e.getClickedBlock().getLocation(), Flags.BOAT);
             }
-            // Spawn eggs
-            else if (e.getItem().getType().name().endsWith("_SPAWN_EGG")) {
-                checkIsland(e, e.getPlayer(), e.getClickedBlock().getLocation(), Flags.SPAWN_EGGS);
+            else if (e.getItem().getType().name().endsWith("_SPAWN_EGG"))
+            {
+                this.checkIsland(e, e.getPlayer(), e.getClickedBlock().getLocation(), Flags.SPAWN_EGGS);
             }
-            // Now check for in-hand items
-            if (e.getItem() != null) {
-                if (e.getItem().getType().name().contains("BOAT")) {
-                    checkIsland(e, e.getPlayer(), e.getClickedBlock().getLocation(), Flags.PLACE_BLOCKS);
-                    return;
-                }
-                switch (e.getItem().getType()) {
-                case ENDER_PEARL:
-                    checkIsland(e, e.getPlayer(), e.getClickedBlock().getLocation(), Flags.ENDER_PEARL);
-                    break;
-                case BONE_MEAL:
-                    checkIsland(e, e.getPlayer(), e.getClickedBlock().getLocation(), Flags.PLACE_BLOCKS);
-                    break;
-                default:
-                    break;
+            else if (e.getItem().getType() == Material.ENDER_PEARL)
+            {
+                this.checkIsland(e, e.getPlayer(), e.getClickedBlock().getLocation(), Flags.ENDER_PEARL);
+            }
+            else if (e.getItem().getType() == Material.BONE_MEAL)
+            {
+                this.checkIsland(e, e.getPlayer(), e.getClickedBlock().getLocation(), Flags.PLACE_BLOCKS);
+            }
+            else if (e.getItem().getType() == Material.GLASS_BOTTLE)
+            {
+                Block targetedBlock = e.getPlayer().getTargetBlockExact(5, FluidCollisionMode.ALWAYS);
+
+                // Check if player is clicking on water or waterlogged block with a bottle.
+                if (targetedBlock != null && (Material.WATER.equals(targetedBlock.getType()) ||
+                    targetedBlock.getBlockData() instanceof Waterlogged))
+                {
+                    this.checkIsland(e, e.getPlayer(), e.getClickedBlock().getLocation(), Flags.BREWING);
                 }
             }
         }
     }
 
+
     /**
      * Check if an action can occur on a clicked block
+     *
      * @param e - event called
      * @param player - player
      * @param loc - location of clicked block
      * @param type - material type of clicked block
      */
-    private void checkClickedBlock(Event e, Player player, Location loc, Material type) {
+    private void checkClickedBlock(Event e, Player player, Location loc, Material type)
+    {
         // Handle pots
-        if (type.name().startsWith("POTTED")) {
-            checkIsland(e, player, loc, Flags.FLOWER_POT);
-            return;
-        }
-        if (Tag.ANVIL.isTagged(type)) {
-            checkIsland(e, player, loc, Flags.ANVIL);
-            return;
-        }
-        if (Tag.BUTTONS.isTagged(type)) {
-            checkIsland(e, player, loc, Flags.BUTTON);
-            return;
-        }
-        if (Tag.BEDS.isTagged(type)) {
-            checkIsland(e, player, loc, Flags.BED);
-            return;
-        }
-        if (Tag.DOORS.isTagged(type)) {
-            checkIsland(e, player, loc, Flags.DOOR);
-            return;
-        }
-        if (Tag.SHULKER_BOXES.isTagged(type)) {
-            checkIsland(e, player, loc, Flags.SHULKER_BOX);
-            return;
-        }
-        if (Tag.TRAPDOORS.isTagged(type)) {
-            checkIsland(e, player, loc, Flags.TRAPDOOR);
+        if (type.name().startsWith("POTTED"))
+        {
+            this.checkIsland(e, player, loc, Flags.FLOWER_POT);
             return;
         }
 
-        switch (type) {
-        case BEACON:
-            checkIsland(e, player, loc, Flags.BEACON);
-            break;
-        case BREWING_STAND:
-        case CAULDRON:
-            checkIsland(e, player, loc, Flags.BREWING);
-            break;
-        case BEEHIVE:
-        case BEE_NEST:
-            checkIsland(e, player, loc, Flags.HIVE);
-            break;
-        case BARREL:
-            checkIsland(e, player, loc, Flags.BARREL);
-            break;
-        case CHEST:
-        case CHEST_MINECART:
-            checkIsland(e, player, loc, Flags.CHEST);
-            break;
-        case TRAPPED_CHEST:
-            checkIsland(e, player, loc, Flags.TRAPPED_CHEST);
-            break;
-        case FLOWER_POT:
-            checkIsland(e, player, loc, Flags.FLOWER_POT);
-            break;
-        case COMPOSTER:
-            checkIsland(e, player, loc, Flags.COMPOSTER);
-            break;
-        case DISPENSER:
-            checkIsland(e, player, loc, Flags.DISPENSER);
-            break;
-        case DROPPER:
-            checkIsland(e, player, loc, Flags.DROPPER);
-            break;
-        case HOPPER:
-        case HOPPER_MINECART:
-            checkIsland(e, player, loc, Flags.HOPPER);
-            break;
-        case BLAST_FURNACE:
-        case CAMPFIRE:
-        case FURNACE_MINECART:
-        case FURNACE:
-        case SMOKER:
-            checkIsland(e, player, loc, Flags.FURNACE);
-            break;
-        case ENCHANTING_TABLE:
-            checkIsland(e, player, loc, Flags.ENCHANTING);
-            break;
-        case ENDER_CHEST:
-            checkIsland(e, player, loc, Flags.ENDER_CHEST);
-            break;
-        case JUKEBOX:
-            checkIsland(e, player, loc, Flags.JUKEBOX);
-            break;
-        case NOTE_BLOCK:
-            checkIsland(e, player, loc, Flags.NOTE_BLOCK);
-            break;
-        case CRAFTING_TABLE:
-        case CARTOGRAPHY_TABLE:
-        case GRINDSTONE:
-        case STONECUTTER:
-        case LOOM:
-            checkIsland(e, player, loc, Flags.CRAFTING);
-            break;
-        case LEVER:
-            checkIsland(e, player, loc, Flags.LEVER);
-            break;
-        case REDSTONE_WIRE:
-        case REPEATER:
-        case COMPARATOR:
-        case DAYLIGHT_DETECTOR:
-            checkIsland(e, player, loc, Flags.REDSTONE);
-            break;
-        case DRAGON_EGG:
-            checkIsland(e, player, loc, Flags.DRAGON_EGG);
-            break;
-        case END_PORTAL_FRAME:
-            checkIsland(e, player, loc, Flags.PLACE_BLOCKS);
-            break;
-        case GLOW_ITEM_FRAME:
-        case ITEM_FRAME:
-            checkIsland(e, player, loc, Flags.ITEM_FRAME);
-            break;
-        case SWEET_BERRY_BUSH:
-            checkIsland(e, player, loc, Flags.BREAK_BLOCKS);
-            break;
-        case CAKE:
-            checkIsland(e, player, loc, Flags.CAKE);
-            break;
-        case OAK_FENCE_GATE:
-        case SPRUCE_FENCE_GATE:
-        case BIRCH_FENCE_GATE:
-        case JUNGLE_FENCE_GATE:
-        case DARK_OAK_FENCE_GATE:
-        case ACACIA_FENCE_GATE:
-        case CRIMSON_FENCE_GATE:
-        case WARPED_FENCE_GATE:
-            checkIsland(e, player, loc, Flags.GATE);
-            break;
-        default:
-            if (stringFlags.containsKey(type.name())) {
-                Optional<Flag> f = BentoBox.getInstance().getFlagsManager().getFlag(stringFlags.get(type.name()));
-                f.ifPresent(flag -> checkIsland(e, player, loc, flag));
+        if (Tag.ANVIL.isTagged(type))
+        {
+            this.checkIsland(e, player, loc, Flags.ANVIL);
+            return;
+        }
+
+        if (Tag.BUTTONS.isTagged(type))
+        {
+            this.checkIsland(e, player, loc, Flags.BUTTON);
+            return;
+        }
+
+        if (Tag.BEDS.isTagged(type))
+        {
+            this.checkIsland(e, player, loc, Flags.BED);
+            return;
+        }
+
+        if (Tag.DOORS.isTagged(type))
+        {
+            this.checkIsland(e, player, loc, Flags.DOOR);
+            return;
+        }
+
+        if (Tag.SHULKER_BOXES.isTagged(type))
+        {
+            this.checkIsland(e, player, loc, Flags.SHULKER_BOX);
+            return;
+        }
+
+        if (Tag.TRAPDOORS.isTagged(type))
+        {
+            this.checkIsland(e, player, loc, Flags.TRAPDOOR);
+            return;
+        }
+
+        if (Tag.FENCE_GATES.isTagged(type))
+        {
+            this.checkIsland(e, player, loc, Flags.GATE);
+        }
+        // TODO: 1.18 compatibility
+        // if (Tag.ITEMS_CHEST_BOATS.isTagged(type)) {
+        //   this.checkIsland(e, player, loc, Flags.CHEST);
+        // }
+
+        switch (type)
+        {
+            case BEACON -> this.checkIsland(e, player, loc, Flags.BEACON);
+            case BREWING_STAND -> this.checkIsland(e, player, loc, Flags.BREWING);
+            case BEEHIVE, BEE_NEST -> this.checkIsland(e, player, loc, Flags.HIVE);
+            case BARREL -> this.checkIsland(e, player, loc, Flags.BARREL);
+            case CHEST, CHEST_MINECART -> this.checkIsland(e, player, loc, Flags.CHEST);
+            case TRAPPED_CHEST -> this.checkIsland(e, player, loc, Flags.TRAPPED_CHEST);
+            case FLOWER_POT -> this.checkIsland(e, player, loc, Flags.FLOWER_POT);
+            case COMPOSTER -> this.checkIsland(e, player, loc, Flags.COMPOSTER);
+            case DISPENSER -> this.checkIsland(e, player, loc, Flags.DISPENSER);
+            case DROPPER -> this.checkIsland(e, player, loc, Flags.DROPPER);
+            case HOPPER, HOPPER_MINECART -> this.checkIsland(e, player, loc, Flags.HOPPER);
+            case BLAST_FURNACE, CAMPFIRE, FURNACE_MINECART, FURNACE, SMOKER ->
+                this.checkIsland(e, player, loc, Flags.FURNACE);
+            case ENCHANTING_TABLE -> this.checkIsland(e, player, loc, Flags.ENCHANTING);
+            case ENDER_CHEST -> this.checkIsland(e, player, loc, Flags.ENDER_CHEST);
+            case JUKEBOX -> this.checkIsland(e, player, loc, Flags.JUKEBOX);
+            case NOTE_BLOCK -> this.checkIsland(e, player, loc, Flags.NOTE_BLOCK);
+            case CRAFTING_TABLE, CARTOGRAPHY_TABLE, GRINDSTONE, STONECUTTER, LOOM ->
+                this.checkIsland(e, player, loc, Flags.CRAFTING);
+            case LEVER -> this.checkIsland(e, player, loc, Flags.LEVER);
+            case REDSTONE_WIRE, REPEATER, COMPARATOR, DAYLIGHT_DETECTOR -> this.checkIsland(e, player, loc, Flags.REDSTONE);
+            case DRAGON_EGG -> this.checkIsland(e, player, loc, Flags.DRAGON_EGG);
+            case END_PORTAL_FRAME, RESPAWN_ANCHOR -> this.checkIsland(e, player, loc, Flags.PLACE_BLOCKS);
+            case GLOW_ITEM_FRAME, ITEM_FRAME -> this.checkIsland(e, player, loc, Flags.ITEM_FRAME);
+            case SWEET_BERRY_BUSH -> this.checkIsland(e, player, loc, Flags.BREAK_BLOCKS);
+            case CAKE -> this.checkIsland(e, player, loc, Flags.CAKE);
+            case LAVA_CAULDRON ->
+            {
+                if (BlockInteractionListener.holds(player, Material.BUCKET))
+                {
+                    this.checkIsland(e, player, loc, Flags.COLLECT_LAVA);
+                }
+            }
+            case WATER_CAULDRON ->
+            {
+                if (BlockInteractionListener.holds(player, Material.BUCKET))
+                {
+                    this.checkIsland(e, player, loc, Flags.COLLECT_WATER);
+                }
+                else if (BlockInteractionListener.holds(player, Material.GLASS_BOTTLE) ||
+                    BlockInteractionListener.holds(player, Material.POTION))
+                {
+                    this.checkIsland(e, player, loc, Flags.BREWING);
+                }
+            }
+            case POWDER_SNOW_CAULDRON ->
+            {
+                if (BlockInteractionListener.holds(player, Material.BUCKET))
+                {
+                    this.checkIsland(e, player, loc, Flags.COLLECT_POWDERED_SNOW);
+                }
+            }
+            case CAULDRON ->
+            {
+                if (BlockInteractionListener.holds(player, Material.WATER_BUCKET) ||
+                    BlockInteractionListener.holds(player, Material.LAVA_BUCKET) ||
+                    BlockInteractionListener.holds(player, Material.POWDER_SNOW_BUCKET))
+                {
+                    this.checkIsland(e, player, loc, Flags.BUCKET);
+                }
+                else if (BlockInteractionListener.holds(player, Material.POTION))
+                {
+                    this.checkIsland(e, player, loc, Flags.BREWING);
+                }
+            }
+            default ->
+            {
+                if (stringFlags.containsKey(type.name()))
+                {
+                    Optional<Flag> f = BentoBox.getInstance().getFlagsManager().getFlag(stringFlags.get(type.name()));
+                    f.ifPresent(flag -> this.checkIsland(e, player, loc, flag));
+                }
             }
         }
     }
 
+
     /**
-     * When breaking blocks is allowed, this protects
-     * specific blocks from being broken, which would bypass the protection.
-     * For example, player enables break blocks, but chests are still protected
-     * Fires after the BreakBlocks check.
+     * When breaking blocks is allowed, this protects specific blocks from being broken, which would bypass the
+     * protection. For example, player enables break blocks, but chests are still protected Fires after the BreakBlocks
+     * check.
      *
      * @param e - event
      */
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
-    public void onBlockBreak(final BlockBreakEvent e) {
-        checkClickedBlock(e, e.getPlayer(), e.getBlock().getLocation(), e.getBlock().getType());
+    public void onBlockBreak(final BlockBreakEvent e)
+    {
+        this.checkClickedBlock(e, e.getPlayer(), e.getBlock().getLocation(), e.getBlock().getType());
     }
+
 
     /**
      * Prevents dragon eggs from flying out of an island's protected space
+     *
      * @param e - event
      */
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onDragonEggTeleport(BlockFromToEvent e) {
+    public void onDragonEggTeleport(BlockFromToEvent e)
+    {
         Block block = e.getBlock();
-        if (!block.getType().equals(Material.DRAGON_EGG) || !getIWM().inWorld(block.getLocation())) {
+
+        if (!block.getType().equals(Material.DRAGON_EGG) || !this.getIWM().inWorld(block.getLocation()))
+        {
             return;
         }
+
         // If egg starts in a protected island...
         // Cancel if toIsland is not fromIsland or if there is no protected island there
         // This protects against eggs dropping into adjacent islands, e.g. island distance and protection range are equal
-        Optional<Island> fromIsland = getIslands().getProtectedIslandAt(block.getLocation());
-        Optional<Island> toIsland = getIslands().getProtectedIslandAt(e.getToBlock().getLocation());
+        Optional<Island> fromIsland = this.getIslands().getProtectedIslandAt(block.getLocation());
+        Optional<Island> toIsland = this.getIslands().getProtectedIslandAt(e.getToBlock().getLocation());
         fromIsland.ifPresent(from -> e.setCancelled(toIsland.map(to -> to != from).orElse(true)));
     }
 
 
+
+
+    /**
+     * This method returns if player is holding given material in main or offhand.
+     * @param player Player that must be checked.
+     * @param material item that mus t be checjed.
+     * @return {@code true} if player is holding item in main hand or offhand.
+     */
+    private static boolean holds(Player player, Material material)
+    {
+        return player.getInventory().getItemInMainHand().getType().equals(material) ||
+            player.getInventory().getItemInOffHand().getType().equals(material);
+    }
 }

src/main/java/world/bentobox/bentobox/listeners/flags/protection/BreakBlocksListener.java
@@ -58,19 +58,23 @@ public class BreakBlocksListener extends FlagListener {
      * @param e - event
      */
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
-    public void onPlayerInteract(final PlayerInteractEvent e) {
+    public void onPlayerInteract(final PlayerInteractEvent e)
+    {
         // Only handle hitting things
-        if (!e.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+        if (!e.getAction().equals(Action.LEFT_CLICK_BLOCK) || e.getClickedBlock() == null)
+        {
             return;
         }
+
         Player p = e.getPlayer();
         Location l = e.getClickedBlock().getLocation();
-        switch (e.getClickedBlock().getType()) {
-        case CAKE -> checkIsland(e, p, l, Flags.BREAK_BLOCKS);
-        case SPAWNER ->  checkIsland(e, p, l, Flags.BREAK_SPAWNERS);
-        case DRAGON_EGG ->  checkIsland(e, p, l, Flags.DRAGON_EGG);
-        case HOPPER -> checkIsland(e, p, l, Flags.BREAK_HOPPERS);
-        default -> {}
+        
+        switch (e.getClickedBlock().getType())
+        {
+            case CAKE -> this.checkIsland(e, p, l, Flags.BREAK_BLOCKS);
+            case SPAWNER -> this.checkIsland(e, p, l, Flags.BREAK_SPAWNERS);
+            case DRAGON_EGG -> this.checkIsland(e, p, l, Flags.DRAGON_EGG);
+            case HOPPER -> this.checkIsland(e, p, l, Flags.BREAK_HOPPERS);
         }
     }
 
@@ -79,16 +83,26 @@ public class BreakBlocksListener extends FlagListener {
      * @param e - event
      */
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled=true)
-    public void onVehicleDamageEvent(VehicleDamageEvent e) {
+    public void onVehicleDamageEvent(VehicleDamageEvent e)
+    {
         Location l = e.getVehicle().getLocation();
-        if (getIWM().inWorld(l) && e.getAttacker() instanceof Player p) {
-            String vehicleType = e.getVehicle().getType().toString();
-            if (e.getVehicle().getType().equals(EntityType.BOAT)) {
-                checkIsland(e, p, l, Flags.BOAT);
-            } else if (vehicleType.contains("MINECART")) {
-                checkIsland(e, p, l, Flags.MINECART);
-            } else {
-                checkIsland(e, p, l, Flags.BREAK_BLOCKS);
+
+        if (getIWM().inWorld(l) && e.getAttacker() instanceof Player p)
+        {
+            String vehicleType = e.getVehicle().getType().name();
+
+            // 1.19 introduced Chest Boat.
+            if (vehicleType.contains("BOAT"))
+            {
+                this.checkIsland(e, p, l, Flags.BOAT);
+            }
+            else if (vehicleType.contains("MINECART"))
+            {
+                this.checkIsland(e, p, l, Flags.MINECART);
+            }
+            else
+            {
+                this.checkIsland(e, p, l, Flags.BREAK_BLOCKS);
             }
         }
     }

commit e41f5ac24f31724634559aa4a4dd07f523437a51

Fixes a bug that prevented mobs to attack
Commit 85b52f4b introduced a bug that prevented entities to target any island member. This should fix it.

src/main/java/world/bentobox/bentobox/listeners/flags/worldsettings/InvincibleVisitorsListener.java
@@ -171,7 +171,7 @@ public class InvincibleVisitorsListener extends FlagListener implements ClickHan
         if (!(e.getTarget() instanceof Player p) ||
             !this.getIWM().inWorld(world) ||
             e.getTarget().hasMetadata("NPC") ||
-            this.getIslands().userIsOnIsland(world, User.getInstance(e.getEntity())) ||
+            this.getIslands().userIsOnIsland(world, User.getInstance(e.getTarget())) ||
             this.PVPAllowed(p.getLocation()) ||
             e.getReason() == EntityTargetEvent.TargetReason.TARGET_DIED ||
             !this.getIWM().getIvSettings(world).contains(DamageCause.ENTITY_ATTACK.name()))