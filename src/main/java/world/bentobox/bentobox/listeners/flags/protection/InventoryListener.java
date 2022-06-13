f01fe2ef65aa560d2af51bc72d4322066ce5a32d
BONNe <bonne@bonne.id.lv>
2022-06-13 12:59:17 +0300

    Add ChestBoat inventory protection. Part of #1987

--- src/main/java/world/bentobox/bentobox/listeners/flags/protection/InventoryListener.java
+++ src/main/java/world/bentobox/bentobox/listeners/flags/protection/InventoryListener.java
@@ -13,6 +13,7 @@ import org.bukkit.block.Furnace;
 import org.bukkit.block.Hopper;
 import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.Animals;
+import org.bukkit.entity.ChestBoat;
 import org.bukkit.entity.NPC;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.minecart.HopperMinecart;
@@ -22,9 +23,10 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.InventoryHolder;
 
-import world.bentobox.bentobox.BentoBox;
 import world.bentobox.bentobox.api.flags.FlagListener;
 import world.bentobox.bentobox.lists.Flags;
+import world.bentobox.bentobox.versions.ServerCompatibility;
+
 
 /**
  * Handles inventory protection
@@ -37,66 +39,97 @@ public class InventoryListener extends FlagListener {
      * @param e - event
      */
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled=true)
-    public void onInventoryClick(InventoryClickEvent e) {
-        Player player = (Player)e.getWhoClicked();
-
+    public void onInventoryClick(InventoryClickEvent e)
+    {
+        Player player = (Player) e.getWhoClicked();
         InventoryHolder inventoryHolder = e.getInventory().getHolder();
-        if (inventoryHolder == null || !(e.getWhoClicked() instanceof Player)) {
+
+        if (inventoryHolder == null || !(e.getWhoClicked() instanceof Player))
+        {
             return;
         }
-        if (inventoryHolder instanceof Animals) {
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.MOUNT_INVENTORY);
+
+        if (inventoryHolder instanceof Animals)
+        {
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.MOUNT_INVENTORY);
         }
-        else if (inventoryHolder instanceof Dispenser) {
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.DISPENSER);
+        else if (inventoryHolder instanceof Dispenser)
+        {
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.DISPENSER);
         }
-        else if (inventoryHolder instanceof Dropper) {
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.DROPPER);
+        else if (inventoryHolder instanceof Dropper)
+        {
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.DROPPER);
         }
-        else if (inventoryHolder instanceof Hopper
-                || inventoryHolder instanceof HopperMinecart) {
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.HOPPER);
+        else if (inventoryHolder instanceof Hopper || inventoryHolder instanceof HopperMinecart)
+        {
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.HOPPER);
         }
-        else if (inventoryHolder instanceof Furnace) {
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.FURNACE);
+        else if (inventoryHolder instanceof Furnace)
+        {
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.FURNACE);
         }
-        else if (inventoryHolder instanceof BrewingStand) {
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.BREWING);
+        else if (inventoryHolder instanceof BrewingStand)
+        {
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.BREWING);
         }
-        else if (inventoryHolder instanceof Beacon) {
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.BEACON);
+        else if (inventoryHolder instanceof Beacon)
+        {
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.BEACON);
         }
-        else if (inventoryHolder instanceof NPC) {
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.TRADING);
+        else if (inventoryHolder instanceof NPC)
+        {
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.TRADING);
         }
-        else if (inventoryHolder instanceof Barrel) {
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.BARREL);
+        else if (inventoryHolder instanceof Barrel)
+        {
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.BARREL);
         }
-        else if (inventoryHolder instanceof ShulkerBox) {
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.SHULKER_BOX);
+        else if (inventoryHolder instanceof ShulkerBox)
+        {
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.SHULKER_BOX);
         }
-        else if (inventoryHolder instanceof Chest c) {
-            checkInvHolder(c.getLocation(), e, player);
+        else if (inventoryHolder instanceof Chest c)
+        {
+            this.checkInvHolder(c.getLocation(), e, player);
         }
-        else if (inventoryHolder instanceof DoubleChest dc) {
-            checkInvHolder(dc.getLocation(), e, player);
+        else if (inventoryHolder instanceof DoubleChest dc)
+        {
+            this.checkInvHolder(dc.getLocation(), e, player);
         }
-        else if (inventoryHolder instanceof StorageMinecart) {
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.CHEST);
+        else if (inventoryHolder instanceof StorageMinecart)
+        {
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.CHEST);
         }
-        else if (!(inventoryHolder instanceof Player)) {
+        else if (!ServerCompatibility.getInstance().isVersion(ServerCompatibility.ServerVersion.V1_18, ServerCompatibility.ServerVersion.V1_18_1, ServerCompatibility.ServerVersion.V1_18_2) &&
+            inventoryHolder instanceof ChestBoat)
+        {
+            // TODO: 1.19 added chest boat. Remove compatibility check when 1.18 is dropped.
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.CHEST);
+        }
+        else if (!(inventoryHolder instanceof Player))
+        {
             // All other containers
-            checkIsland(e, player, e.getInventory().getLocation(), Flags.CONTAINER);
+            this.checkIsland(e, player, e.getInventory().getLocation(), Flags.CONTAINER);
         }
     }
 
-    private void checkInvHolder(Location l, InventoryClickEvent e, Player player) {
-        if (l.getBlock().getType().equals(Material.TRAPPED_CHEST)) {
-            checkIsland(e, player, l, Flags.TRAPPED_CHEST);
-        } else {
-            checkIsland(e, player, l, Flags.CHEST);
+
+    /**
+     * This method runs check based on clicked chest type.
+     * @param l location of chest.
+     * @param e click event.
+     * @param player player who clicked.
+     */
+    private void checkInvHolder(Location l, InventoryClickEvent e, Player player)
+    {
+        if (l.getBlock().getType().equals(Material.TRAPPED_CHEST))
+        {
+            this.checkIsland(e, player, l, Flags.TRAPPED_CHEST);
         }
-
-
+        else
+        {
+            this.checkIsland(e, player, l, Flags.CHEST);
+        }
     }
 }
