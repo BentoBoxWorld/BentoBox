927fcba15a3a4c7ccf728b89d92c33aa3d651088
BONNe <bonne@bonne.id.lv>
2022-07-07 00:08:15 +0300

    Disables biome copying by default in blueprints. Biome will be copied only if it is required by copy command parameter.
    Fixes #1862

--- src/main/java/world/bentobox/bentobox/api/commands/admin/blueprints/AdminBlueprintCopyCommand.java
+++ src/main/java/world/bentobox/bentobox/api/commands/admin/blueprints/AdminBlueprintCopyCommand.java
@@ -1,35 +1,54 @@
 package world.bentobox.bentobox.api.commands.admin.blueprints;
 
 import java.util.List;
+import java.util.Optional;
 
 import world.bentobox.bentobox.api.commands.CompositeCommand;
 import world.bentobox.bentobox.api.user.User;
 import world.bentobox.bentobox.blueprints.BlueprintClipboard;
 
-public class AdminBlueprintCopyCommand extends CompositeCommand {
 
-    public AdminBlueprintCopyCommand(AdminBlueprintCommand parent) {
+public class AdminBlueprintCopyCommand extends CompositeCommand
+{
+    public AdminBlueprintCopyCommand(AdminBlueprintCommand parent)
+    {
         super(parent, "copy");
     }
 
+
     @Override
-    public void setup() {
+    public void setup()
+    {
         inheritPermission();
         setParametersHelp("commands.admin.blueprint.copy.parameters");
         setDescription("commands.admin.blueprint.copy.description");
     }
 
+
     @Override
-    public boolean execute(User user, String label, List<String> args) {
-        if (args.size() > 1) {
-            showHelp(this, user);
+    public boolean execute(User user, String label, List<String> args)
+    {
+        if (args.size() > 2)
+        {
+            this.showHelp(this, user);
             return false;
         }
 
         AdminBlueprintCommand parent = (AdminBlueprintCommand) getParent();
 
-        BlueprintClipboard clipboard = parent.getClipboards().computeIfAbsent(user.getUniqueId(), v -> new BlueprintClipboard());
-        boolean copyAir = (args.size() == 1 && args.get(0).equalsIgnoreCase("air"));
-        return clipboard.copy(user, copyAir);
+        BlueprintClipboard clipboard =
+            parent.getClipboards().computeIfAbsent(user.getUniqueId(), v -> new BlueprintClipboard());
+
+        boolean copyAir = args.stream().anyMatch(key -> key.equalsIgnoreCase("air"));
+        boolean copyBiome = args.stream().anyMatch(key -> key.equalsIgnoreCase("biome"));
+
+        return clipboard.copy(user, copyAir, copyBiome);
+    }
+
+
+    @Override
+    public Optional<List<String>> tabComplete(User user, String alias, List<String> args)
+    {
+        return Optional.of(List.of("air", "biome"));
     }
 }
