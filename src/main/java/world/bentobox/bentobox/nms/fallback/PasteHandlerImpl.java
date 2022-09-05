2b68458f1a42b6fc8c8049805f03c29055395b87
HSGamer <huynhqtienvtag@gmail.com>
2022-09-05 11:19:10 +0700

    use collectingAndThen toArray

--- src/main/java/world/bentobox/bentobox/nms/fallback/PasteHandlerImpl.java
+++ src/main/java/world/bentobox/bentobox/nms/fallback/PasteHandlerImpl.java
@@ -11,21 +11,30 @@ import world.bentobox.bentobox.util.DefaultPasteUtil;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class PasteHandlerImpl implements PasteHandler {
     @Override
     public CompletableFuture<Void> pasteBlocks(Island island, World world, Map<Location, BlueprintBlock> blockMap) {
         return blockMap.entrySet().parallelStream()
                 .map(entry -> DefaultPasteUtil.setBlock(island, entry.getKey(), entry.getValue()))
-                .reduce((left, right) -> left.thenCombine(right, (a, b) -> null))
-                .orElse(CompletableFuture.completedFuture(null));
+                .collect(
+                        Collectors.collectingAndThen(
+                                Collectors.toList(),
+                                list -> CompletableFuture.allOf(list.toArray(new CompletableFuture[0]))
+                        )
+                );
     }
 
     @Override
     public CompletableFuture<Void> pasteEntities(Island island, World world, Map<Location, List<BlueprintEntity>> entityMap) {
         return entityMap.entrySet().parallelStream()
                 .map(entry -> DefaultPasteUtil.setEntity(island, entry.getKey(), entry.getValue()))
-                .reduce((left, right) -> left.thenCombine(right, (a, b) -> null))
-                .orElse(CompletableFuture.completedFuture(null));
+                .collect(
+                        Collectors.collectingAndThen(
+                                Collectors.toList(),
+                                list -> CompletableFuture.allOf(list.toArray(new CompletableFuture[0]))
+                        )
+                );
     }
 }
