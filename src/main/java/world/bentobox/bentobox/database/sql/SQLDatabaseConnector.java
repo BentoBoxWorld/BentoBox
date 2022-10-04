369001d368bcb80c19dac10b79088677f9e7bd61
BONNe <bonne@bonne.id.lv>
2022-10-04 12:31:51 +0300

    Rework SQL database implementation. (#1993)
    This change switches from plain JDBC driver implementation to a HikariCP Pool implementation.
    
    Pool Implementation is complete for PostgreSQL and SQLite, while MariaDB and MySQL implementation still uses JDBC drivers, however with HikariCP pools.
    
    Also, I added extra properties for SQL databases, where users could specify their own datasource properties.

--- src/main/java/world/bentobox/bentobox/database/sql/SQLDatabaseConnector.java
+++ src/main/java/world/bentobox/bentobox/database/sql/SQLDatabaseConnector.java
@@ -1,7 +1,8 @@
 package world.bentobox.bentobox.database.sql;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,61 +13,130 @@ import org.eclipse.jdt.annotation.NonNull;
 import world.bentobox.bentobox.database.DatabaseConnectionSettingsImpl;
 import world.bentobox.bentobox.database.DatabaseConnector;
 
-public abstract class SQLDatabaseConnector implements DatabaseConnector {
 
+/**
+ * Generic SQL database connector.
+ */
+public abstract class SQLDatabaseConnector implements DatabaseConnector
+{
+    /**
+     * The connection url string for the sql database.
+     */
     protected String connectionUrl;
-    private final DatabaseConnectionSettingsImpl dbSettings;
-    protected static Connection connection = null;
+
+    /**
+     * The database connection settings.
+     */
+    protected final DatabaseConnectionSettingsImpl dbSettings;
+
+    /**
+     * Hikari Data Source that creates all connections.
+     */
+    protected static HikariDataSource dataSource;
+
+    /**
+     * Type of objects stored in database.
+     */
     protected static Set<Class<?>> types = new HashSet<>();
 
-    protected SQLDatabaseConnector(DatabaseConnectionSettingsImpl dbSettings, String connectionUrl) {
+
+    /**
+     * Default connector constructor.
+     * @param dbSettings Settings of the database.
+     * @param connectionUrl Connection url for the database.
+     */
+    protected SQLDatabaseConnector(DatabaseConnectionSettingsImpl dbSettings, String connectionUrl)
+    {
         this.dbSettings = dbSettings;
         this.connectionUrl = connectionUrl;
     }
 
+
+    /**
+     * Returns connection url of database.
+     * @return Database connection url.
+     */
     @Override
-    public String getConnectionUrl() {
+    public String getConnectionUrl()
+    {
         return connectionUrl;
     }
 
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @NonNull
-    public String getUniqueId(String tableName) {
+    public String getUniqueId(String tableName)
+    {
         // Not used
         return "";
     }
 
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public boolean uniqueIdExists(String tableName, String key) {
+    public boolean uniqueIdExists(String tableName, String key)
+    {
         // Not used
         return false;
     }
 
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void closeConnection(Class<?> type) {
+    public void closeConnection(Class<?> type)
+    {
         types.remove(type);
-        if (types.isEmpty() && connection != null) {
-            try {
-                connection.close();
-                Bukkit.getLogger().info("Closed database connection");
-            } catch (SQLException e) {
-                Bukkit.getLogger().severe("Could not close database connection");
-            }
+
+        if (types.isEmpty())
+        {
+            dataSource.close();
+            Bukkit.getLogger().info("Closed database connection");
         }
     }
 
+
+    /**
+     * This method creates config that is used to create HikariDataSource.
+     * @return HikariConfig object.
+     */
+    public abstract HikariConfig createConfig();
+
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public Object createConnection(Class<?> type) {
+    public Object createConnection(Class<?> type)
+    {
         types.add(type);
+
         // Only make one connection to the database
-        if (connection == null) {
-            try {
-                connection = DriverManager.getConnection(connectionUrl, dbSettings.getUsername(), dbSettings.getPassword());
-            } catch (SQLException e) {
+        if (dataSource == null)
+        {
+            try
+            {
+                dataSource = new HikariDataSource(this.createConfig());
+
+                // Test connection
+                try (Connection connection = dataSource.getConnection())
+                {
+                    connection.isValid(5 * 1000);
+                }
+            }
+            catch (SQLException e)
+            {
                 Bukkit.getLogger().severe("Could not connect to the database! " + e.getMessage());
+                dataSource = null;
             }
         }
-        return connection;
-    }
 
+        return dataSource;
+    }
 }
--- src/main/java/world/bentobox/bentobox/database/sql/mysql/MySQLDatabaseHandler.java
+++ src/main/java/world/bentobox/bentobox/database/sql/mysql/MySQLDatabaseHandler.java
@@ -13,17 +13,21 @@ import world.bentobox.bentobox.database.sql.SQLDatabaseHandler;
  *
  * @param <T>
  */
-public class MySQLDatabaseHandler<T> extends SQLDatabaseHandler<T> {
-
+public class MySQLDatabaseHandler<T> extends SQLDatabaseHandler<T>
+{
     /**
-     * Handles the connection to the database and creation of the initial database schema (tables) for
-     * the class that will be stored.
+     * Handles the connection to the database and creation of the initial database schema (tables) for the class that
+     * will be stored.
+     *
      * @param plugin - plugin object
      * @param type - the type of class to be stored in the database. Must inherit DataObject
      * @param dbConnecter - authentication details for the database
      */
-    MySQLDatabaseHandler(BentoBox plugin, Class<T> type, DatabaseConnector dbConnecter) {
-        super(plugin, type, dbConnecter, new SQLConfiguration(plugin, type)
-                .schema("CREATE TABLE IF NOT EXISTS `[tableName]` (json JSON, uniqueId VARCHAR(255) GENERATED ALWAYS AS (json->\"$.uniqueId\"), UNIQUE INDEX i (uniqueId) ) ENGINE = INNODB"));
+    MySQLDatabaseHandler(BentoBox plugin, Class<T> type, DatabaseConnector dbConnecter)
+    {
+        super(plugin,
+            type,
+            dbConnecter,
+            new SQLConfiguration(plugin, type).schema("CREATE TABLE IF NOT EXISTS `[tableName]` (json JSON, uniqueId VARCHAR(255) GENERATED ALWAYS AS (json->\"$.uniqueId\"), UNIQUE INDEX i (uniqueId) ) ENGINE = INNODB"));
     }
 }
