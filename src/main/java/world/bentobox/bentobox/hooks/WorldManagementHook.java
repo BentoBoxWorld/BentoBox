a30584245dde9848155e9cd5814332389b376aa6
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2022-10-13 19:21:58 +0200

    Register BentoBox World Generator option with MyWorlds
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@ziggo.nl>

---
+++ src/main/java/world/bentobox/bentobox/hooks/WorldManagementHook.java
@@ -0,0 +1,20 @@
+package world.bentobox.bentobox.hooks;
+
+import org.bukkit.World;
+
+/**
+ * Hook for a type of Multi-World management plugin that must be made
+ * aware of the correct configuration of a BentoBox World.
+ *
+ * @author bergerkiller (Irmo van den Berge)
+ */
+public interface WorldManagementHook {
+
+    /**
+     * Register the world with the World Management hook
+     *
+     * @param world - world to register
+     * @param islandWorld - if true, then this is an island world
+     */
+    void registerWorld(World world, boolean islandWorld);
+}
