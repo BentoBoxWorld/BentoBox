7aa1e2da8925e3abcaa1e8a6245288c1e08b9a52
tastybento <tastybento@wasteofplastic.com>
2022-11-26 17:36:19 -0800

    Fix to handle kelp and bamboo spread.
    Addresses #2053. This is actually due to a server bug
    https://hub.spigotmc.org/jira/browse/SPIGOT-5312. This change uses the
    BlockSpreadEvent to trap kelp and bamboo and bamboo sapling.

--- src/main/java/world/bentobox/bentobox/listeners/flags/worldsettings/OfflineGrowthListener.java
+++ src/main/java/world/bentobox/bentobox/listeners/flags/worldsettings/OfflineGrowthListener.java
@@ -3,9 +3,11 @@ package world.bentobox.bentobox.listeners.flags.worldsettings;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockGrowEvent;
+import org.bukkit.event.block.BlockSpreadEvent;
 
 import world.bentobox.bentobox.api.flags.FlagListener;
 import world.bentobox.bentobox.lists.Flags;
@@ -22,6 +24,28 @@ public class OfflineGrowthListener extends FlagListener {
     public void onCropGrow(BlockGrowEvent e) {
         if (!getIWM().inWorld(e.getBlock().getWorld()) || Flags.OFFLINE_GROWTH.isSetForWorld(e.getBlock().getWorld())) {
             // We do not want to run any check if this is not the right world or if it is allowed.
+            return;
+        }
+        // Check if island exists and members are online
+        getIslands().getProtectedIslandAt(e.getBlock().getLocation()).ifPresent(i -> {
+            for (UUID uuid : i.getMemberSet(RanksManager.COOP_RANK)) {
+                if (Bukkit.getPlayer(uuid) != null) {
+                    return;
+                }
+            }
+            e.setCancelled(true);
+        });
+    }
+
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onSpread(BlockSpreadEvent e) {
+        if (!getIWM().inWorld(e.getBlock().getWorld()) || Flags.OFFLINE_GROWTH.isSetForWorld(e.getBlock().getWorld())) {
+            // We do not want to run any check if this is not the right world or if it is allowed.
+            return;
+        }
+        // Check what is spreading - disallow Bamboo and Kelp growth
+        Material m = e.getBlock().getType();
+        if (!m.equals(Material.KELP) && !m.equals(Material.BAMBOO) && !m.equals(Material.BAMBOO_SAPLING)) {
             return;
         }
         // Check if island exists and members are online
