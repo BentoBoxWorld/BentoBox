8f954090e7d0ee3dcf2041f29f44d2c8300f2282
tastybento <tastybento@wasteofplastic.com>
2023-05-13 09:20:47 -0700

    Fixes VisitorKeepInventory to disregard coops and trusted
    #2126
    Also fixes some tests.

--- src/test/java/world/bentobox/bentobox/listeners/flags/worldsettings/VisitorKeepInventoryListenerTest.java
+++ src/test/java/world/bentobox/bentobox/listeners/flags/worldsettings/VisitorKeepInventoryListenerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
@@ -122,7 +123,7 @@ public class VisitorKeepInventoryListenerTest {
         /* Islands */
         when(plugin.getIslands()).thenReturn(islandsManager);
         // Visitor
-        when(island.getMemberSet()).thenReturn(ImmutableSet.of());
+        when(island.getMemberSet(anyInt())).thenReturn(ImmutableSet.of());
         // By default, there should be an island.
         when(islandsManager.getProtectedIslandAt(any())).thenReturn(Optional.of(island));
 
