4200fe4abb8940eb003ffa335bffaaf231bce7c7
BONNe <bonne@bonne.id.lv>
2023-10-30 09:17:26 +0200

    Fixes a bug that removed old owner from island (#2218)
    * Fixes a bug that removed old owner from island
    
    These changes fix a bug that was introduced by clearing the island cache from the old owner for 2.0 verison.
    
    This will set the previous owner as sub-owner on the island.
    
    * Fixes rank on event fired.
    
    Changes from Visitor to Sub owner rank
    
    * Update IslandsManager.java
    
    Removes line that removes player from cache. 
    
    They should remain in the cache.
    
    ---------
    
    Co-authored-by: tastybento <tastybento@users.noreply.github.com>

--- src/main/java/world/bentobox/bentobox/managers/IslandsManager.java
+++ src/main/java/world/bentobox/bentobox/managers/IslandsManager.java
@@ -1520,8 +1520,8 @@ public class IslandsManager {
      */
     public void setOwner(User user, UUID targetUUID, Island island) {
         islandCache.setOwner(island, targetUUID);
-        // Remove the old owner from the island
-        plugin.getIslands().removePlayer(island, user.getUniqueId());
+        // Set old owner as sub-owner on island.
+        island.setRank(user, RanksManager.SUB_OWNER_RANK);
 
         user.sendMessage("commands.island.team.setowner.name-is-the-owner", "[name]", plugin.getPlayers().getName(targetUUID));
         plugin.getIWM().getAddon(island.getWorld()).ifPresent(addon -> {
