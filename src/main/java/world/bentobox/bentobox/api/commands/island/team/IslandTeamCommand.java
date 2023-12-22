29a7fa551a970e62e6d1b7d96efd121681f07e2d
tastybento <tastybento@users.noreply.github.com>
2023-12-23 08:36:27 +0900

    WIP - add GUI for teams.

--- src/main/java/world/bentobox/bentobox/api/commands/island/team/IslandTeamCommand.java
+++ src/main/java/world/bentobox/bentobox/api/commands/island/team/IslandTeamCommand.java
@@ -1,7 +1,10 @@
 package world.bentobox.bentobox.api.commands.island.team;
 
+import java.io.File;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,14 +12,21 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 
+import world.bentobox.bentobox.BentoBox;
 import world.bentobox.bentobox.api.commands.CompositeCommand;
 import world.bentobox.bentobox.api.events.IslandBaseEvent;
 import world.bentobox.bentobox.api.events.team.TeamEvent;
 import world.bentobox.bentobox.api.localization.TextVariables;
+import world.bentobox.bentobox.api.panels.PanelItem;
+import world.bentobox.bentobox.api.panels.TemplatedPanel;
+import world.bentobox.bentobox.api.panels.builders.PanelItemBuilder;
+import world.bentobox.bentobox.api.panels.builders.TemplatedPanelBuilder;
+import world.bentobox.bentobox.api.panels.reader.ItemTemplateRecord;
 import world.bentobox.bentobox.api.user.User;
 import world.bentobox.bentobox.database.objects.Island;
 import world.bentobox.bentobox.managers.RanksManager;
@@ -25,10 +35,20 @@ import world.bentobox.bentobox.util.Util;
 public class IslandTeamCommand extends CompositeCommand {
 
     /**
+     * List of ranks that we will loop through in order
+     */
+    private static final List<Integer> RANKS = List.of(RanksManager.OWNER_RANK, RanksManager.SUB_OWNER_RANK,
+            RanksManager.MEMBER_RANK, RanksManager.TRUSTED_RANK, RanksManager.COOP_RANK);
+
+    /**
      * Invited list. Key is the invited party, value is the invite.
      * @since 1.8.0
      */
     private final Map<UUID, Invite> inviteMap;
+
+    private User user;
+
+    private Island island;
 
     public IslandTeamCommand(CompositeCommand parent) {
         super(parent, "team");
@@ -57,12 +77,16 @@ public class IslandTeamCommand extends CompositeCommand {
         }
         new IslandTeamPromoteCommand(this, "promote");
         new IslandTeamPromoteCommand(this, "demote");
+
+        // Panels
+        getPlugin().saveResource("panels/team_panel.yml", false);
     }
 
     @Override
     public boolean execute(User user, String label, List<String> args) {
+        this.user = user;
         // Player issuing the command must have an island
-        Island island = getIslands().getPrimaryIsland(getWorld(), user.getUniqueId());
+        island = getIslands().getPrimaryIsland(getWorld(), user.getUniqueId());
         if (island == null) {
             user.sendMessage("general.errors.no-island");
             return false;
@@ -85,85 +109,216 @@ public class IslandTeamCommand extends CompositeCommand {
             }
         }
         // Show members of island
-        showMembers(island, user);
+        showMembers().forEach(user::sendRawMessage);
+        build();
         return true;
     }
 
-    private void showMembers(Island island, User user) {
+    /**
+     * This method builds this GUI.
+     */
+    private void build() {
+        // Start building panel.
+        TemplatedPanelBuilder panelBuilder = new TemplatedPanelBuilder();
+        panelBuilder.user(user);
+        panelBuilder.world(user.getWorld());
+
+        panelBuilder.template("team_panel", new File(getPlugin().getDataFolder(), "panels"));
+
+        panelBuilder.parameters("[name]", user.getName(), "[display_name]", user.getDisplayName());
+
+        panelBuilder.registerTypeBuilder("STATUS", this::createStatusButton);
+        panelBuilder.registerTypeBuilder("MEMBER", this::createMemberButton);
+        //panelBuilder.registerTypeBuilder("INVITE", this::createInviteButton);
+        //panelBuilder.registerTypeBuilder("KICK", this::createKickButton);
+
+        // Register unknown type builder.
+        panelBuilder.build();
+    }
+
+    /**
+     * Create status button panel item.
+     *
+     * @param template the template
+     * @param slot     the slot
+     * @return the panel item
+     */
+    private PanelItem createStatusButton(ItemTemplateRecord template, TemplatedPanel.ItemSlot slot) {
+        PanelItemBuilder builder = new PanelItemBuilder();
+        // Player issuing the command must have an island
+        Island island = getIslands().getPrimaryIsland(getWorld(), user.getUniqueId());
+        if (island == null) {
+            return builder.icon(Material.BARRIER).name(user.getTranslation("general.errors.no-island")).build();
+        }
+
+        return builder.icon(user.getName()).name(user.getTranslation("commands.island.team.gui.buttons.status.name"))
+                .description(showMembers()).build();
+    }
+
+    /**
+     * Create member button panel item.
+     *
+     * @param template the template
+     * @param slot     the slot
+     * @return the panel item
+     */
+    private PanelItem createMemberButton(ItemTemplateRecord template, TemplatedPanel.ItemSlot slot) {
+        PanelItemBuilder builder = new PanelItemBuilder();
+        // Player issuing the command must have an island
+        Island island = getIslands().getPrimaryIsland(getWorld(), user.getUniqueId());
+        if (island == null) {
+            return builder.icon(Material.BARRIER).name(user.getTranslation("general.errors.no-island")).build();
+        }
+        if (slot.slot() == 0 && island.getOwner() != null) {
+            // Owner
+            User owner = User.getInstance(island.getOwner());
+            return builder.icon(owner.getName())
+                    .name(owner.getDisplayName())
+                    .description(user.getTranslation("commands.island.team.info.rank-layout.owner", TextVariables.RANK,
+                            user.getTranslation(RanksManager.OWNER_RANK_REF)))
+                    .build();
+        }
+        long subOwnerCount = island.getMemberSet(RanksManager.SUB_OWNER_RANK, false).stream().count();
+        long memberCount = island.getMemberSet(RanksManager.MEMBER_RANK, false).stream().count();
+        long coopCount = island.getMemberSet(RanksManager.COOP_RANK, false).stream().count();
+        long trustedCount = island.getMemberSet(RanksManager.TRUSTED_RANK, false).stream().count();
+        BentoBox.getInstance().logDebug("Slot = " + slot.slot());
+
+        if (slot.slot() > 0 && slot.slot() < subOwnerCount + 1) {
+            // Show sub owners
+            PanelItem item = getMemberButton(RanksManager.SUB_OWNER_RANK_REF, RanksManager.SUB_OWNER_RANK,
+                    subOwnerCount, slot.slot());
+            if (item != null) {
+                return item;
+            }
+
+        }
+        if (slot.slot() > subOwnerCount && slot.slot() < subOwnerCount + memberCount + 1) {
+            // Show members 
+            PanelItem item = getMemberButton(RanksManager.MEMBER_RANK_REF, RanksManager.MEMBER_RANK, memberCount,
+                    slot.slot());
+            if (item != null) {
+                return item;
+            }
+        }
+        if (slot.slot() > subOwnerCount + memberCount && slot.slot() < subOwnerCount + memberCount + trustedCount + 1) {
+            // Show trusted
+            PanelItem item = getMemberButton(RanksManager.TRUSTED_RANK_REF, RanksManager.TRUSTED_RANK, trustedCount,
+                    slot.slot());
+            if (item != null) {
+                return item;
+            }
+
+        }
+        if (slot.slot() > subOwnerCount + memberCount + trustedCount
+                && slot.slot() < subOwnerCount + memberCount + trustedCount + coopCount + 1) {
+            // Show coops
+            PanelItem item = getMemberButton(RanksManager.COOP_RANK_REF, RanksManager.COOP_RANK, coopCount,
+                    slot.slot());
+            if (item != null) {
+                return item;
+            }
+
+        }
+        return builder.icon(Material.BLACK_STAINED_GLASS_PANE).name("&b&r").build();
+    }
+
+    private PanelItem getMemberButton(String ref, int rank, long count, int slot) {
+        BentoBox.getInstance().logDebug(ref + " " + rank + " count = " + count + " slot = " + slot);
+        User player = island.getMemberSet(rank, false).stream().sorted().skip(slot - 1).limit(1L)
+                .map(User::getInstance).findFirst().orElse(null);
+        if (player != null) {
+            return new PanelItemBuilder().icon(player.getName()).name(player.getDisplayName())
+                    .description(user.getTranslation("commands.island.team.info.rank-layout.generic",
+                            TextVariables.RANK, user.getTranslation(ref), TextVariables.NUMBER, String.valueOf(count)))
+                    .build();
+        } else {
+            BentoBox.getInstance().logDebug("no player found");
+        }
+        return null;
+    }
+
+    private List<String> showMembers() {
+        List<String> message = new ArrayList<>();
         // Gather online members
-        long count = island.getMemberSet(RanksManager.MEMBER_RANK).stream()
+        long onlineMemberCount = island.getMemberSet(RanksManager.MEMBER_RANK).stream()
                 .filter(uuid -> Util.getOnlinePlayerList(user).contains(Bukkit.getOfflinePlayer(uuid).getName()))
                 .count();
 
-        // List of ranks that we will loop through
-        Integer[] ranks = new Integer[] { RanksManager.OWNER_RANK, RanksManager.SUB_OWNER_RANK,
-                RanksManager.MEMBER_RANK, RanksManager.TRUSTED_RANK, RanksManager.COOP_RANK };
-
         // Show header:
-        user.sendMessage("commands.island.team.info.header", "[max]",
+        message.add(user.getTranslation("commands.island.team.info.header", "[max]",
                 String.valueOf(getIslands().getMaxMembers(island, RanksManager.MEMBER_RANK)), "[total]",
-                String.valueOf(island.getMemberSet().size()), "[online]", String.valueOf(count));
+                String.valueOf(island.getMemberSet().size()), "[online]", String.valueOf(onlineMemberCount)));
 
         // We now need to get all online "members" of the island - incl. Trusted and coop
         List<UUID> onlineMembers = island.getMemberSet(RanksManager.COOP_RANK).stream()
                 .filter(uuid -> Util.getOnlinePlayerList(user).contains(Bukkit.getOfflinePlayer(uuid).getName()))
                 .toList();
 
-        for (int rank : ranks) {
+        for (int rank : RANKS) {
             Set<UUID> players = island.getMemberSet(rank, false);
             if (!players.isEmpty()) {
                 if (rank == RanksManager.OWNER_RANK) {
                     // Slightly special handling for the owner rank
-                    user.sendMessage("commands.island.team.info.rank-layout.owner", TextVariables.RANK,
-                            user.getTranslation(RanksManager.OWNER_RANK_REF));
+                    message.add(user.getTranslation("commands.island.team.info.rank-layout.owner", TextVariables.RANK,
+                            user.getTranslation(RanksManager.OWNER_RANK_REF)));
                 } else {
-                    user.sendMessage("commands.island.team.info.rank-layout.generic", TextVariables.RANK,
+                    message.add(user.getTranslation("commands.island.team.info.rank-layout.generic", TextVariables.RANK,
                             user.getTranslation(getPlugin().getRanksManager().getRank(rank)), TextVariables.NUMBER,
-                            String.valueOf(island.getMemberSet(rank, false).size()));
+                            String.valueOf(island.getMemberSet(rank, false).size())));
                 }
-                displayOnOffline(user, rank, island, onlineMembers);
+                message.addAll(displayOnOffline(user, rank, island, onlineMembers));
             }
+        }
+        return message;
+    }
+
+    private List<String> displayOnOffline(User user, int rank, Island island, List<UUID> onlineMembers) {
+        List<String> message = new ArrayList<>();
+        for (UUID member : island.getMemberSet(rank, false)) {
+            message.add(getMemberStatus(user, member, onlineMembers.contains(member)));
+
+        }
+        return message;
+    }
+
+    private String getMemberStatus(User user2, UUID member, boolean online) {
+        OfflinePlayer offlineMember = Bukkit.getOfflinePlayer(member);
+        if (online) {
+            return user.getTranslation("commands.island.team.info.member-layout.online", TextVariables.NAME,
+                    offlineMember.getName());
+        } else {
+            return offlinePlayerStatus(user, offlineMember);
         }
     }
 
-    private void displayOnOffline(User user, int rank, Island island, List<UUID> onlineMembers) {
-        for (UUID member : island.getMemberSet(rank, false)) {
-            OfflinePlayer offlineMember = Bukkit.getOfflinePlayer(member);
-            if (onlineMembers.contains(member)) {
-                // the player is online
-                user.sendMessage("commands.island.team.info.member-layout.online", TextVariables.NAME,
-                        offlineMember.getName());
-            } else {
-                // A bit of handling for the last joined date
-                Instant lastJoined = Instant.ofEpochMilli(offlineMember.getLastPlayed());
-                Instant now = Instant.now();
+    private String offlinePlayerStatus(User user2, OfflinePlayer offlineMember) {
+        // A bit of handling for the last joined date
+        Instant lastJoined = Instant.ofEpochMilli(offlineMember.getLastPlayed());
+        Instant now = Instant.now();
 
-                Duration duration = Duration.between(lastJoined, now);
-                String lastSeen;
-                final String reference = "commands.island.team.info.last-seen.layout";
-                if (duration.toMinutes() < 60L) {
-                    lastSeen = user.getTranslation(reference, TextVariables.NUMBER,
-                            String.valueOf(duration.toMinutes()), TextVariables.UNIT,
-                            user.getTranslation("commands.island.team.info.last-seen.minutes"));
-                } else if (duration.toHours() < 24L) {
-                    lastSeen = user.getTranslation(reference, TextVariables.NUMBER, String.valueOf(duration.toHours()),
-                            TextVariables.UNIT, user.getTranslation("commands.island.team.info.last-seen.hours"));
-                } else {
-                    lastSeen = user.getTranslation(reference, TextVariables.NUMBER, String.valueOf(duration.toDays()),
-                            TextVariables.UNIT, user.getTranslation("commands.island.team.info.last-seen.days"));
-                }
-
-                if (island.getMemberSet(RanksManager.MEMBER_RANK, true).contains(member)) {
-                    user.sendMessage("commands.island.team.info.member-layout.offline", TextVariables.NAME,
-                            offlineMember.getName(), "[last_seen]", lastSeen);
-                } else {
-                    // This will prevent anyone that is trusted or below to not have a last-seen status
-                    user.sendMessage("commands.island.team.info.member-layout.offline-not-last-seen",
-                            TextVariables.NAME, offlineMember.getName());
-                }
-            }
+        Duration duration = Duration.between(lastJoined, now);
+        String lastSeen;
+        final String reference = "commands.island.team.info.last-seen.layout";
+        if (duration.toMinutes() < 60L) {
+            lastSeen = user.getTranslation(reference, TextVariables.NUMBER, String.valueOf(duration.toMinutes()),
+                    TextVariables.UNIT, user.getTranslation("commands.island.team.info.last-seen.minutes"));
+        } else if (duration.toHours() < 24L) {
+            lastSeen = user.getTranslation(reference, TextVariables.NUMBER, String.valueOf(duration.toHours()),
+                    TextVariables.UNIT, user.getTranslation("commands.island.team.info.last-seen.hours"));
+        } else {
+            lastSeen = user.getTranslation(reference, TextVariables.NUMBER, String.valueOf(duration.toDays()),
+                    TextVariables.UNIT, user.getTranslation("commands.island.team.info.last-seen.days"));
         }
 
+        if (island.getMemberSet(RanksManager.MEMBER_RANK, true).contains(offlineMember.getUniqueId())) {
+            return user.getTranslation("commands.island.team.info.member-layout.offline", TextVariables.NAME,
+                    offlineMember.getName(), "[last_seen]", lastSeen);
+        } else {
+            // This will prevent anyone that is trusted or below to not have a last-seen status
+            return user.getTranslation("commands.island.team.info.member-layout.offline-not-last-seen",
+                    TextVariables.NAME, offlineMember.getName());
+        }
     }
 
     private boolean fireEvent(User user, Island island) {
