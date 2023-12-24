9f116e75c4c2f05f012932004cb60b46d08a0214
tastybento <tastybento@users.noreply.github.com>
2023-12-24 13:17:16 +0900

    Simplify hook

--- src/main/java/world/bentobox/bentobox/hooks/SlimefunHook.java
+++ src/main/java/world/bentobox/bentobox/hooks/SlimefunHook.java
@@ -3,10 +3,7 @@ package world.bentobox.bentobox.hooks;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.block.Block;
 
-import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import world.bentobox.bentobox.api.hooks.Hook;
 
@@ -15,8 +12,6 @@ import world.bentobox.bentobox.api.hooks.Hook;
  */
 public class SlimefunHook extends Hook {
 
-    private Slimefun sfPlugin;
-
     public SlimefunHook() {
         super("Slimefun", Material.SLIME_BLOCK);
     }
@@ -24,24 +19,12 @@ public class SlimefunHook extends Hook {
     @Override
     public boolean hook() {
         // See if Slimefun is around
-        sfPlugin = (Slimefun) Bukkit.getPluginManager().getPlugin("SlimeFun");
-        return sfPlugin != null;
+        return Bukkit.getPluginManager().getPlugin("SlimeFun") != null;
     }
 
     @Override
     public String getFailureCause() {
         return ""; // No errors
-    }
-
-    public void clearAllBlockInfoAtChunk(World world, int x, int z, boolean destroy) {
-        if (!BlockStorage.isWorldLoaded(world)) {
-            return; // Not sure if this is needed.
-        }
-        BlockStorage.clearAllBlockInfoAtChunk(world, x, z, destroy);
-    }
-
-    public void clearBlockInfo(Block b, boolean destroy) {
-        BlockStorage.clearBlockInfo(b, destroy);
     }
 
     public void clearBlockInfo(Location location, boolean destroy) {
