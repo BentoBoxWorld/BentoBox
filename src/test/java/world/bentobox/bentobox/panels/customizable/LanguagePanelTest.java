3be034bfa987afced6584af2189c1b1db3440274
BONNe <bonne@bonne.id.lv>
2024-01-04 10:09:44 +0200

    Customizable Language Selection Panel (#2254)
    * Customizable Language Selection Panel
    
    This implements customizable Language Selection Panel. By default, panel is generated in `/plugins/bentobox/panels` folder, however, if GameModeAddon has a specific panel defined in their folder, then that panel is taken.
    
    * Migrate LanguagePanelTest to customizable panel.
    
    * Remove original LanguagePanel
    
    * Save language_panel.yml on server starting.
    
    * Fixes some bugs in LanguagePanel.

--- src/test/java/world/bentobox/bentobox/panels/customizable/LanguagePanelTest.java
+++ src/test/java/world/bentobox/bentobox/panels/customizable/LanguagePanelTest.java
@@ -1,28 +1,15 @@
-package world.bentobox.bentobox.panels;
+package world.bentobox.bentobox.panels.customizable;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.awt.Panel;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFactory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,13 +23,22 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
+import java.awt.Panel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
-import net.md_5.bungee.api.ChatColor;
 import world.bentobox.bentobox.BentoBox;
+import world.bentobox.bentobox.api.addons.GameModeAddon;
+import world.bentobox.bentobox.api.commands.CompositeCommand;
 import world.bentobox.bentobox.api.localization.BentoBoxLocale;
 import world.bentobox.bentobox.api.panels.builders.PanelBuilder;
 import world.bentobox.bentobox.api.user.User;
 import world.bentobox.bentobox.managers.LocalesManager;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 /**
  * @author tastybento
@@ -62,18 +58,22 @@ public class LanguagePanelTest {
     private ArrayList<Locale> localeList;
 
     @Mock
-    private PanelBuilder pb;
-    @Mock
-    private Panel panel;
-    @Mock
     private Inventory inv;
     @Mock
     private ItemMeta meta;
+
+    @Mock
+    private CompositeCommand command;
 
     @Captor
     private ArgumentCaptor<ItemStack> argument;
 
     private Map<Locale, BentoBoxLocale> map;
+
+    /**
+     * Location of the resources folder
+     */
+    private final Path resourcePath = Paths.get("src","test","resources");
 
     /**
      */
@@ -90,7 +90,24 @@ public class LanguagePanelTest {
         when(user.getPlayer()).thenReturn(player);
         when(user.hasPermission(anyString())).thenReturn(true);
         when(user.getTranslation(any())).thenAnswer((Answer<String>) invocation -> invocation.getArgument(0, String.class));
+        when(user.getTranslation(any(World.class), any(), any())).thenAnswer((Answer<String>) invocation -> invocation.getArgument(1, String.class));
+        when(user.getTranslation(any(String.class), any())).thenAnswer((Answer<String>) invocation -> invocation.getArgument(0, String.class));
+        when(user.getTranslationOrNothing(any(), any())).thenAnswer((Answer<String>) invocation -> invocation.getArgument(0, String.class));
         when(user.getLocale()).thenReturn(Locale.ENGLISH);
+
+        when(user.getTranslation(any(World.class), eq("panels.language.buttons.language.name"), any())).
+            thenAnswer((Answer<String>) invocation -> invocation.getArgument(3, String.class));
+
+        GameModeAddon addon = mock(GameModeAddon.class);
+        when(command.getAddon()).thenReturn(addon);
+        when(addon.getDataFolder()).thenReturn(resourcePath.toFile());
+
+        World world = mock(World.class);
+        when(command.getWorld()).thenReturn(world);
+
+        when(plugin.getDescription()).thenAnswer((Answer<PluginDescriptionFile>) invocation ->
+            new PluginDescriptionFile("BentoBox", "1.0", "world.bentobox.bentobox"));
+
         User.setPlugin(plugin);
         // Set up user already
         User.getInstance(player);
@@ -123,17 +140,17 @@ public class LanguagePanelTest {
     }
 
     /**
-     * Test method for {@link world.bentobox.bentobox.panels.LanguagePanel#openPanel(world.bentobox.bentobox.api.user.User)}.
+     * Test method for {@link world.bentobox.bentobox.panels.customizable.LanguagePanel#openPanel(world.bentobox.bentobox.api.commands.CompositeCommand,world.bentobox.bentobox.api.user.User)}.
      */
     @Test
     public void testOpenPanelNoLocales() {
-        LanguagePanel.openPanel(user);
+        LanguagePanel.openPanel(command, user);
         verify(plugin).getLocalesManager();
         verify(lm).getAvailableLocales(eq(true));
     }
 
     /**
-     * Test method for {@link world.bentobox.bentobox.panels.LanguagePanel#openPanel(world.bentobox.bentobox.api.user.User)}.
+     * Test method for {@link world.bentobox.bentobox.panels.customizable.LanguagePanel#openPanel(world.bentobox.bentobox.api.commands.CompositeCommand,world.bentobox.bentobox.api.user.User)}.
      */
     @Test
     public void testOpenPanelLocalesNullBanner() {
@@ -146,29 +163,30 @@ public class LanguagePanelTest {
         map.put(Locale.CHINA, bbl);
         map.put(Locale.ENGLISH, bbl);
 
-        LanguagePanel.openPanel(user);
+        LanguagePanel.openPanel(command, user);
         verify(lm, times(3)).getLanguages();
         verify(bbl, times(3)).getBanner();
-        verify(user).getTranslation("language.panel-title");
+        verify(user).getTranslation("panels.language.title");
         // Other langs
-        verify(user, times(2)).getTranslation(eq("language.description.click-to-select"));
-        verify(user, times(3)).getTranslation(eq("language.description.authors"));
-        // Selected language
-        verify(user, Mockito.atMostOnce()).getTranslation(eq("language.description.selected"));
+        verify(user, times(3)).getTranslation(eq("panels.language.buttons.language.authors"));
+        verify(user, times(1)).getTranslation(eq("panels.language.buttons.language.selected"));
+        verify(user, times(3)).getTranslationOrNothing(eq("panels.language.buttons.language.description"), any());
+        verify(user, times(2)).getTranslation(any(World.class), eq("panels.tips.click-to-choose"));
 
         verify(inv).setItem(eq(0), argument.capture());
         assertEquals(Material.WHITE_BANNER, argument.getValue().getType());
         assertEquals(1, argument.getValue().getAmount());
         assertEquals(meta, argument.getValue().getItemMeta());
-        verify(meta).setDisplayName(eq(ChatColor.WHITE + "Chinese (China)"));
-        verify(meta).setDisplayName(eq(ChatColor.WHITE + "English (Canada)"));
+
+        verify(meta).setDisplayName(eq("Chinese (China)"));
+        verify(meta).setDisplayName(eq("English (Canada)"));
         verify(inv).setItem(eq(1), any());
         verify(inv).setItem(eq(2), any());
         verify(inv, Mockito.never()).setItem(eq(3), any());
     }
 
     /**
-     * Test method for {@link world.bentobox.bentobox.panels.LanguagePanel#openPanel(world.bentobox.bentobox.api.user.User)}.
+     * Test method for {@link world.bentobox.bentobox.panels.customizable.LanguagePanel#openPanel(world.bentobox.bentobox.api.commands.CompositeCommand,world.bentobox.bentobox.api.user.User)}.
      */
     @Test
     public void testOpenPanelLocalesNotNullBanner() {
@@ -178,7 +196,7 @@ public class LanguagePanelTest {
         map.put(Locale.CANADA, bbl);
         when(bbl.getBanner()).thenReturn(new ItemStack(Material.CYAN_BANNER));
 
-        LanguagePanel.openPanel(user);
+        LanguagePanel.openPanel(command, user);
         verify(inv).setItem(eq(0), argument.capture());
         assertEquals(Material.CYAN_BANNER, argument.getValue().getType());
     }
