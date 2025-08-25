f41ce1b7bc8bda14d3704bdf68d2ebb465fedbd0
Ben Gibbs <ben@afero.io>
2025-08-25 15:03:55 -0700

    Revert "Load islands even if they are marked as deleted."
    This reverts commit c1ddc743d8397f60642ae0d15428d6e7162371fb.

--- src/main/java/world/bentobox/bentobox/managers/IslandsManager.java
+++ src/main/java/world/bentobox/bentobox/managers/IslandsManager.java
@@ -1306,9 +1306,8 @@ public class IslandsManager {
             if (island.isDeleted()) {
                 // These will be deleted later
                 deletedIslands.add(island.getUniqueId());
-            }
-            // Check island distance and if incorrect stop BentoBox
-            if (!plugin.getSettings().isOverrideSafetyCheck() && island.getWorld() != null
+            } // Check island distance and if incorrect stop BentoBox
+            else if (!plugin.getSettings().isOverrideSafetyCheck() && island.getWorld() != null
                     && plugin.getIWM().inWorld(island.getWorld())
                     && island.getRange() != plugin.getIWM().getIslandDistance(island.getWorld())) {
                 throw new IOException("Island distance mismatch!\n" + "World '" + island.getWorld().getName()
