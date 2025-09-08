7a5f46c35fdbc70134bea886a642d200e5229b69
tastybento <tastybento@users.noreply.github.com>
2025-09-07 17:08:23 -0700

    Improve based on static analysis

--- src/main/java/world/bentobox/bentobox/api/commands/CompositeCommand.java
+++ src/main/java/world/bentobox/bentobox/api/commands/CompositeCommand.java
@@ -360,7 +360,7 @@ public abstract class CompositeCommand extends Command implements PluginIdentifi
     }
 
     /**
-     * @return this command's sub-level. Top level is 0. Every time a command
+     * @return this command's sublevel. Top level is 0. Every time a command
      *         registers with a parent, their level will be set.
      */
     protected int getLevel() {
@@ -581,7 +581,6 @@ public abstract class CompositeCommand extends Command implements PluginIdentifi
     /**
      * Sets locale reference to this command's description. It is used to display
      * the help of this command.
-     *
      * <br/>
      * <br/>
      *
@@ -619,7 +618,6 @@ public abstract class CompositeCommand extends Command implements PluginIdentifi
     /**
      * Sets locale reference to this command's parameters. It is used to display the
      * help of this command.
-     *
      * <br/>
      * <br/>
      *
@@ -782,7 +780,7 @@ public abstract class CompositeCommand extends Command implements PluginIdentifi
     }
 
     /**
-     * The the world that this command applies to.
+     * The world that this command applies to.
      * 
      * @return the world
      */
