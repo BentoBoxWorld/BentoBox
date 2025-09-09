2ba14bdd1470d92be9e9ec9c7c665a5d97cc483f
tastybento <tastybento@users.noreply.github.com>
2025-09-08 19:45:40 -0700

    Improve based on static analysis

--- src/main/java/world/bentobox/bentobox/listeners/flags/worldsettings/CreeperListener.java
+++ src/main/java/world/bentobox/bentobox/listeners/flags/worldsettings/CreeperListener.java
@@ -45,7 +45,7 @@ public class CreeperListener extends FlagListener {
         // Check for griefing
         Creeper creeper = (Creeper)e.getEntity();
         if (!Flags.CREEPER_GRIEFING.isSetForWorld(e.getLocation().getWorld())
-                && creeper.getTarget() instanceof Player target // if getTarget is null this won'e be true
+                && creeper.getTarget() instanceof Player target // if getTarget is null this won't be true
                 && !getIslands().locationIsOnIsland(target, e.getLocation())) {
             User user = User.getInstance(target);
             user.notify("protection.protected", TextVariables.DESCRIPTION, user.getTranslation(Flags.CREEPER_GRIEFING.getHintReference()));
