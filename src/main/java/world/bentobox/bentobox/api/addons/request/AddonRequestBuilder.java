7bd0cc86dfc625fdaf60a81671591d1dac1263c7
tastybento <tastybento@users.noreply.github.com>
2025-12-14 12:35:26 -0800

    WIP - cracked the way to do it using Paper API only.

--- src/main/java/world/bentobox/bentobox/api/addons/request/AddonRequestBuilder.java
+++ src/main/java/world/bentobox/bentobox/api/addons/request/AddonRequestBuilder.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import org.apache.commons.lang.Validate;
+import org.apache.commons.lang3.Validate;
 
 import world.bentobox.bentobox.BentoBox;
 import world.bentobox.bentobox.api.addons.Addon;
---
+++ src/main/java/world/bentobox/bentobox/nms/v1_21_11_R0_1_SNAPSHOT/GetMetaData.java
@@ -0,0 +1,26 @@
+package world.bentobox.bentobox.nms.v1_21_11_R0_1_SNAPSHOT;
+
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.CraftWorld;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import world.bentobox.bentobox.nms.AbstractMetaData;
+
+
+public class GetMetaData extends AbstractMetaData {
+
+    @Override
+    public String nmsData(Block block) {
+        ServerLevel sl = ((CraftWorld) block.getWorld()).getHandle();
+        BlockEntity te = sl.getBlockEntity(new BlockPos(block.getX(), block.getY(), block.getZ()));
+
+        ClientboundBlockEntityDataPacket packet = (ClientboundBlockEntityDataPacket) te.getUpdatePacket();
+        CompoundTag nbtTag = packet.getTag();
+
+        return nbtTag.toString();
+    }
+}
