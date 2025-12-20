dbacd10b01b1b7d43151963196d59da33f38d563
tastybento <tastybento@users.noreply.github.com>
2025-12-20 16:13:10 +0000

    Revert class loading change.
    It results in concurrent map errors

--- src/main/java/world/bentobox/bentobox/api/addons/AddonClassLoader.java
+++ src/main/java/world/bentobox/bentobox/api/addons/AddonClassLoader.java
@@ -218,19 +218,31 @@ public class AddonClassLoader extends URLClassLoader {
         if (name.startsWith("world.bentobox.bentobox")) {
             return null;
         }
-        return classes.computeIfAbsent(name, key -> {
-            Class<?> resolved = checkGlobal ? loader.getClassByName(key) : null;
-            if (resolved != null) {
-                return resolved;
+        // Check local cache first.
+        Class<?> result = classes.get(name);
+        if (result == null) {
+            // Check global cache for classes from other addons.
+            if (checkGlobal) {
+                result = loader.getClassByName(name);
             }
-            try {
-                resolved = AddonClassLoader.super.findClass(key);
-                loader.setClass(key, resolved);
-                return resolved;
-            } catch (ClassNotFoundException | NoClassDefFoundError e) {
-                return null; // Not found locally.
+
+            if (result == null) {
+                // Try to find the class in this addon's jar.
+                try {
+                    result = super.findClass(name);
+                } catch (ClassNotFoundException | NoClassDefFoundError e) {
+                    // Do nothing. The class is not in this jar.
+                }
+                if (result != null) {
+                    // Class found in this addon's jar, so add it to the global cache.
+                    loader.setClass(name, result);
+
+                }
             }
-        });
+            // Add the class to the local cache.
+            classes.put(name, result);
+        }
+        return result;
     }
 
     /**
