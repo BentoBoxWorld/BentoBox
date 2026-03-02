b134e51b5cab1ff6b67a4762f369831cfec4cd92
tastybento <tastybento@users.noreply.github.com>
2026-03-01 22:01:09 -0800

    Add Disabled annotation to HurtingListenerTest

--- src/test/java/world/bentobox/bentobox/listeners/flags/protection/HurtingListenerTest.java
+++ src/test/java/world/bentobox/bentobox/listeners/flags/protection/HurtingListenerTest.java
@@ -22,6 +22,7 @@ import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerFishEvent.State;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
